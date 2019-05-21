428a8447abaf07231c7861cba96861e9d56ea158
Anton Ovchinnikov <anton@tonyo.info>
2019-05-22 00:41:07 +0200

    feat: Split premake into multiple files

---
+++ premake/premake5.sentrypad.lua
@@ -0,0 +1,115 @@
+workspace "Sentrypad"
+
+function sentrypad_common()
+  language "C++"
+  cppdialect "C++14"
+  includedirs {
+    SRC_ROOT.."/include",
+  }
+
+  filter "system:macosx or linux"
+    toolset("clang")
+  filter {}
+end
+
+project "sentry_crashpad"
+  kind "SharedLib"
+  sentrypad_common()
+  defines {"SENTRY_CRASHPAD"}
+  includedirs {
+    CRASHPAD_PKG,
+    CRASHPAD_PKG.."/include",
+    CRASHPAD_PKG.."/include/mini_chromium",
+  }
+
+  libdirs {
+    CRASHPAD_PKG.."/lib",
+  }
+
+  files {
+    SRC_ROOT.."/src/sentry.cpp",
+    SRC_ROOT.."/src/crashpad_backend.cpp",
+    SRC_ROOT.."/src/vendor/mpack.c",
+  }
+
+  postbuildcommands {
+    "{COPY} "..CRASHPAD_PKG.."/bin/crashpad_handler %{cfg.buildtarget.directory}",
+  }
+
+  -- Crashpad
+  links {
+    "client", "base", "util"
+  }
+
+  filter "system:macosx"
+    -- System
+    links {
+      "Foundation.framework",
+      "Security.framework",
+      "CoreText.framework",
+      "CoreGraphics.framework",
+      "IOKit.framework",
+      "bsm",
+    }
+  filter "system:linux"
+    links {
+    }
+
+-- project "sentry_breakpad"
+--   kind "SharedLib"
+--   defines {"SENTRY_BREAKPAD"}
+--   buildoptions {
+--     "-fvisibility=hidden",
+--   }
+--   includedirs {
+--     BREAKPAD_PKG.."/include",
+--   }
+
+--   libdirs {
+--     BREAKPAD_PKG.."/lib",
+--   }
+
+--   files {
+--     SRC_ROOT.."/src/sentry.cpp",
+--     SRC_ROOT.."/src/breakpad_backend.cpp",
+--     SRC_ROOT.."/src/vendor/mpack.c",
+--   }
+
+--   -- Breakpad
+--   links {
+--     "breakpad_client",
+--   }
+
+--   filter "system:macosx"
+--     -- System
+--     links {
+--       "Foundation.framework",
+--       "pthread",
+--     }
+--   filter "system:linux"
+--     -- System
+--     links {
+--       "pthread",
+--     }
+--   filter {}
+
+project "example_crashpad"
+  kind "ConsoleApp"
+  links {"sentry_crashpad"}
+  buildoptions {
+    "-fPIC",
+  }
+  files {
+    SRC_ROOT.."/example.c",
+  }
+
+
+project "example_breakpad"
+  kind "ConsoleApp"
+  links {"sentry_breakpad", "dl"}
+  buildoptions {
+    "-fPIC",
+  }
+  files {
+    SRC_ROOT.."/example.c",
+  }
