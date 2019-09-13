92e6e92e12cd9a22759779a3fa2d2d5a284c55d9
Armin Ronacher <armin.ronacher@active-4.com>
2019-09-13 09:55:52 +0200

    fix: Version check for macos

--- src/unwinders/backtrace.cpp
+++ src/unwinders/backtrace.cpp
@@ -6,7 +6,7 @@ using namespace sentry;
 
 size_t unwinders::unwind_stack(void *addr, void **ptrs, size_t max_frames) {
     if (addr) {
-        #if MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_14
+        #ifdef MAC_OS_X_VERSION_10_14
         return backtrace_from_fp(addr, ptrs, max_frames);
         #endif
         return 0;
