674296112651bfdd6f4214ccb641e8ccd594c0d3
Armin Ronacher <armin.ronacher@active-4.com>
2019-10-30 07:51:25 +0100

    ref: change how signal handlers are set up

--- src/backends/inproc_backend.cpp
+++ src/backends/inproc_backend.cpp
@@ -1,6 +1,8 @@
 #include "inproc_backend.hpp"
 #ifdef SENTRY_WITH_INPROC_BACKEND
 
+#include "../unwind.hpp"
+
 using namespace sentry;
 using namespace backends;
 
@@ -12,6 +14,8 @@ struct SignalSlot {
     const char *signame;
     const char *sigdesc;
 };
+
+static const size_t MAX_FRAMES = 128;
 
 // we need quite a bit of space for backtrace generation
 static const size_t SIGNAL_COUNT = 6;
@@ -55,7 +59,14 @@ static void invoke_signal_handler(int signum,
 }
 
 static void handle_signal(int signum, siginfo_t *info, void *user_context) {
-    // do something useful here
+    sentry_ucontext_t uctx;
+    uctx.siginfo = info;
+    uctx.user_context = (ucontext_t *)user_context;
+
+    void *backtrace[MAX_FRAMES];
+    size_t frames = unwinders::unwind_stack_backtrace(
+        nullptr, &uctx, &backtrace[0], MAX_FRAMES);
+
     reset_signal_handlers();
     invoke_signal_handler(signum, info, user_context);
 }
@@ -64,9 +75,18 @@ InprocBackend::InprocBackend() {
     g_signal_stack.ss_sp = malloc(SIGNAL_STACK_SIZE);
     g_signal_stack.ss_size = SIGNAL_STACK_SIZE;
     g_signal_stack.ss_flags = 0;
+    memset(g_previous_handlers, 0, sizeof(g_previous_handlers));
+    sigemptyset(&g_sigaction.sa_mask);
+    g_sigaction.sa_sigaction = handle_signal;
+    g_sigaction.sa_flags = SA_SIGINFO | SA_ONSTACK;
+}
+
+InprocBackend::~InprocBackend() {
+}
+
+void InprocBackend::start() {
     sigaltstack(&g_signal_stack, 0);
 
-    memset(g_previous_handlers, 0, sizeof(g_previous_handlers));
     for (size_t i = 0; i < SIGNAL_COUNT; ++i) {
         if (sigaction(SIGNAL_DEFINITIONS[i].signum, nullptr,
                       &g_previous_handlers[i]) == -1) {
@@ -74,19 +94,12 @@ InprocBackend::InprocBackend() {
         }
     }
 
-    sigemptyset(&g_sigaction.sa_mask);
-    g_sigaction.sa_sigaction = handle_signal;
-    g_sigaction.sa_flags = SA_SIGINFO | SA_ONSTACK;
-
     for (size_t i = 0; i < SIGNAL_COUNT; ++i) {
         sigaction(SIGNAL_DEFINITIONS[i].signum, &g_sigaction, nullptr);
     }
-}
 
-InprocBackend::~InprocBackend() {
-}
-
-void InprocBackend::start() {
+    // not that any of this matters ...
+    std::atomic_signal_fence(std::memory_order_release);
 }
 
 void InprocBackend::flush_scope_state(const sentry::Scope &scope) {
