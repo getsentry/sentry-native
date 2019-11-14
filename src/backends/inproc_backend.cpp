bbe2dec26a4b843970e68469bf45fb2c92a8c53d
Armin Ronacher <armin.ronacher@active-4.com>
2019-11-14 12:25:13 +0100

    fix: inproc backend

--- src/backends/inproc_backend.cpp
+++ src/backends/inproc_backend.cpp
@@ -110,7 +110,7 @@ static void handle_signal(int signum, siginfo_t *info, void *user_context) {
         values.append(exc);
         event.set_by_key("exception", exceptions);
 
-        Scope::with_scope([&event](Scope &scope) {
+        Scope::with_scope([&event](const Scope &scope) {
             scope.apply_to_event(event, SENTRY_SCOPE_ALL);
         });
 
