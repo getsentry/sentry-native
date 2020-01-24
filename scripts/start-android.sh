087e3a861c6f4a05435d6391411b40e7c16290aa
Arpad Borsos <arpad.borsos@sentry.io>
2020-01-24 15:49:02 +0100

    try running Android with API 18

--- scripts/start-android.sh
+++ scripts/start-android.sh
@@ -4,8 +4,9 @@
 # https://docs.microsoft.com/en-us/azure/devops/pipelines/ecosystems/android?view=azure-devops#test-on-the-android-emulator
 
 ARCH=${ANDROID_ARCH:-"x86"}
+API_LEVEL=${ANDROID_API:-"29"}
 AVD_EMULATOR_NAME="sentry_android_${ARCH}"
-IMAGE=${ANDROID_IMAGE:-"system-images;android-27;google_apis;${ARCH}"}
+IMAGE=${ANDROID_IMAGE:-"system-images;android-${API_LEVEL};google_apis;${ARCH}"}
 
 # Create an Android Virtual Device
 echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n $AVD_EMULATOR_NAME -k "$IMAGE" --force
