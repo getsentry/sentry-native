f661f5d17d8ca9ee38315ac77f61f002f0594724
Arpad Borsos <arpad.borsos@sentry.io>
2020-01-24 16:49:27 +0100

    ci: Create a fresh azure config for the C version (#130)
    * start over with a basically empty CI setup
    
    * remove obsolete files, optimize fetching
    
    * add windows and android build commands
    
    * playing around with azure pipeline artifacts again
    
    * make sure android SDK things are correctly installed and invoked
    
    * fix android tests
    
    * fix elf parsing when compiled with outdated gcc
    
    * test with different android NDK versions
    
    * try running Android with API 18

--- tests/test_symbolizer.c
+++ tests/test_symbolizer.c
@@ -15,8 +15,10 @@ asserter(const sentry_frame_info_t *info, void *data)
 {
     int *called = data;
     TEST_CHECK(!!info->symbol);
-    TEST_CHECK(strstr(info->symbol, "test_function") != 0);
-    TEST_CHECK(strstr(info->object_name, "sentry_tests") != 0);
+    TEST_CHECK(!!info->object_name);
+    TEST_CHECK(info->symbol && strstr(info->symbol, "test_function") != 0);
+    TEST_CHECK(
+        info->object_name && strstr(info->object_name, "sentry_tests") != 0);
     TEST_CHECK(info->symbol_addr == &test_function);
     TEST_CHECK(info->instruction_addr == ((char *)(void *)&test_function) + 1);
     *called += 1;
--- vendor/premake-androidmk/README.sentry.md
+++
@@ -1,5 +0,0 @@
-# Sentry Changes
-
-The original project is https://github.com/Meoo/premake-androidmk
-
-Minor changes are made in the following (forked) project: https://github.com/getsentry/premake-androidmk
