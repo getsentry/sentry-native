d0dd4e1abda921d38f15780dc7baff1742690216
Arpad Borsos <arpad.borsos@sentry.io>
2020-03-10 12:39:14 +0100

    ref: Expose SENTRY_SDK_VERSION in public header

--- src/sentry_core.h
+++ src/sentry_core.h
@@ -7,9 +7,6 @@
 #include "sentry_path.h"
 #include "sentry_utils.h"
 
-#define SENTRY_SDK_NAME "sentry-native"
-#define SENTRY_SDK_VERSION "0.2.0"
-#define SENTRY_SDK_USER_AGENT (SENTRY_SDK_NAME "/" SENTRY_SDK_VERSION)
 #define SENTRY_BREADCRUMBS_MAX 100
 
 #ifdef SENTRY_PLATFORM_ANDROID
