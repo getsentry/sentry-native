b4b6a15dbbd3f4f5f516b75a7fd942a61e64895d
Arpad Borsos <arpad.borsos@googlemail.com>
2020-03-12 12:46:28 +0100

    make test output more verbose

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,7 +26,7 @@ class Unittests:
         cmake(dir, ["sentry_test_unit"], ["SENTRY_BACKEND=none"])
         self.dir = dir
     def run(self, test):
-        run(self.dir, "sentry_test_unit", [test], check=True)
+        run(self.dir, "sentry_test_unit", ["--quiet", test], check=True)
 
 @pytest.fixture(scope="session")
 def unittests(tmp_path_factory):
