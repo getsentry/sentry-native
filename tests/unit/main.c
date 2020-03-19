bfbc12e2eeea3e8b7e1db026ecb42a445a1b6daf
Guillaume VARA <guillaume.vara@gmail.com>
2020-03-19 21:31:49 +0100

    try to fix unit tests

--- tests/unit/main.c
+++ tests/unit/main.c
@@ -3,13 +3,13 @@
 #include "sentry_testsupport.h"
 
 #define XX(Name) void CONCAT(test_sentry_, Name)(void);
-#include "tests.inc"
+#include "failing_tests_mingw.inc"
 #undef XX
 
 TEST_LIST = {
 #define DECLARE_TEST(Name, Func) { Name, Func },
 #define XX(Name) DECLARE_TEST(#Name, CONCAT(test_sentry_, Name))
-#include "tests.inc"
+#include "failing_tests_mingw.inc"
 #undef XX
 #undef DECLARE_TEST
     { 0 }
