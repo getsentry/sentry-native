854bd9d78c45a4e598aabc818a5d5bfa6c9984fc
Mikhail Paulyshka <me@mixaill.tk>
2020-06-01 00:54:13 +0300

    feat: implement logger hook

--- src/sentry_core.h
+++ src/sentry_core.h
@@ -2,31 +2,9 @@
 #define SENTRY_CORE_H_INCLUDED
 
 #include "sentry_boot.h"
+#include "sentry_logger.h"
 
 #define SENTRY_BREADCRUMBS_MAX 100
-
-#ifdef SENTRY_PLATFORM_ANDROID
-#    include <android/log.h>
-#    define SENTRY_FPRINTF(fd, message, ...)                                   \
-        __android_log_print(                                                   \
-            ANDROID_LOG_INFO, "sentry-native", message, __VA_ARGS__)
-#else
-#    define SENTRY_FPRINTF(fd, message, ...)                                   \
-        fprintf(fd, "[sentry] " message, __VA_ARGS__)
-#endif
-#define SENTRY_DEBUGF(message, ...)                                            \
-    do {                                                                       \
-        const sentry_options_t *_options = sentry_get_options();               \
-        if (_options && sentry_options_get_debug(_options)) {                  \
-            SENTRY_FPRINTF(stderr, message "\n", __VA_ARGS__);                 \
-        }                                                                      \
-    } while (0)
-
-#define SENTRY_DEBUG(message) SENTRY_DEBUGF("%s", message "")
-
-// TODO: we might want to have different log levels at some point
-#define SENTRY_TRACEF(message, ...) SENTRY_DEBUGF(message, __VA_ARGS__)
-#define SENTRY_TRACE(message) SENTRY_DEBUG(message)
 
 #if defined(__GNUC__) && (__GNUC__ >= 4)
 #    define MUST_USE __attribute__((warn_unused_result))
---
+++ src/sentry_logger.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+
+#include "sentry_logger.h"
+#include "sentry_options.h"
+
+#if defined(SENTRY_PLATFORM_ANDROID)
+
+#    include <android/log.h>
+void
+sentry__logger_defaultlogger(
+    sentry_level_t level, const char *message, va_list args)
+{
+    android_LogPriority priority = ANDROID_LOG_UNKNOWN;
+    switch (level) {
+    case SENTRY_LEVEL_DEBUG:
+        priority = ANDROID_LOG_DEBUG;
+        break;
+    case SENTRY_LEVEL_INFO:
+        priority = ANDROID_LOG_INFO;
+        break;
+    case SENTRY_LEVEL_WARNING:
+        priority = ANDROID_LOG_WARN;
+        break;
+    case SENTRY_LEVEL_ERROR:
+        priority = ANDROID_LOG_ERROR;
+        break;
+    case SENTRY_LEVEL_FATAL:
+        priority = ANDROID_LOG_FATAL;
+        break;
+    default:
+        break;
+    }
+    __android_log_vprint(priority, "sentry-native", message, args);
+}
+
+#else
+
+void
+sentry__logger_defaultlogger(
+    sentry_level_t level, const char *message, va_list args)
+{
+    const char *prefix = "[sentry] ";
+    const char *priority = sentry__logger_describe(level);
+
+    char *format = sentry_malloc(
+        strlen(prefix) + strlen(priority) + strlen(message) + 1);
+    strcpy(format, prefix);
+    strcat(format, priority);
+    strcat(format, message);
+
+    vfprintf(stderr, format, args);
+
+    sentry_free(format);
+}
+
+#endif
+
+const char *
+sentry__logger_describe(sentry_level_t level)
+{
+    switch (level) {
+    case SENTRY_LEVEL_DEBUG:
+        return "DEBUG ";
+    case SENTRY_LEVEL_INFO:
+        return "INFO ";
+    case SENTRY_LEVEL_WARNING:
+        return "WARN ";
+    case SENTRY_LEVEL_ERROR:
+        return "ERROR ";
+    case SENTRY_LEVEL_FATAL:
+        return "FATAL ";
+    default:
+        return "UNKNOWN ";
+    }
+}
+
+void
+sentry__logger_log(sentry_level_t level, const char *message, ...)
+{
+    const sentry_options_t *options = sentry_get_options();
+    if (options && options->logger && sentry_options_get_debug(options)) {
+
+        va_list args;
+        va_start(args, message);
+
+        options->logger(level, message, args);
+
+        va_end(args);
+    }
+}
