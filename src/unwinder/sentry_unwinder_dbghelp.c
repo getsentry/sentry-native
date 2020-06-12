e83927bbfe562dcfc5565dad6e5cacd718bc7b37
Chris Rudd <rudd.chris.m@gmail.com>
2020-06-12 07:21:49 -0500

    fix: ContextRecord incorrectly passed to StackWalk64 (#294)

--- src/unwinder/sentry_unwinder_dbghelp.c
+++ src/unwinder/sentry_unwinder_dbghelp.c
@@ -38,21 +38,21 @@ sentry__unwind_stack_dbghelp(
 
     sentry__init_dbghelp();
 
-    CONTEXT *ctx = uctx->exception_ptrs.ContextRecord;
+    CONTEXT ctx = *uctx->exception_ptrs.ContextRecord;
     STACKFRAME64 stack_frame;
     memset(&stack_frame, 0, sizeof(stack_frame));
 
     size_t size = 0;
 #if defined(_WIN64)
     int machine_type = IMAGE_FILE_MACHINE_AMD64;
-    stack_frame.AddrPC.Offset = ctx->Rip;
-    stack_frame.AddrFrame.Offset = ctx->Rbp;
-    stack_frame.AddrStack.Offset = ctx->Rsp;
+    stack_frame.AddrPC.Offset = ctx.Rip;
+    stack_frame.AddrFrame.Offset = ctx.Rbp;
+    stack_frame.AddrStack.Offset = ctx.Rsp;
 #else
     int machine_type = IMAGE_FILE_MACHINE_I386;
-    stack_frame.AddrPC.Offset = ctx->Eip;
-    stack_frame.AddrFrame.Offset = ctx->Ebp;
-    stack_frame.AddrStack.Offset = ctx->Esp;
+    stack_frame.AddrPC.Offset = ctx.Eip;
+    stack_frame.AddrFrame.Offset = ctx.Ebp;
+    stack_frame.AddrStack.Offset = ctx.Esp;
 #endif
     stack_frame.AddrPC.Mode = AddrModeFlat;
     stack_frame.AddrFrame.Mode = AddrModeFlat;
