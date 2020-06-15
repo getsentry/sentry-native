ba594309e089fec8069411138b6bbc6153b03aba
Arpad Borsos <arpad.borsos@sentry.io>
2020-06-15 17:44:49 +0200

    meta: Fix for craft not passing the old version for bumping

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -14,6 +14,9 @@ else
   exit 1
 fi
 
+echo "Current version: ${OLD_VERSION}"
+echo "Bumping version: ${NEW_VERSION}"
+
 sed -i '' -e "s/^#define SENTRY_SDK_VERSION.*/#define SENTRY_SDK_VERSION \"${NEW_VERSION}\"/" include/sentry.h
-sed -i '' -e "s/\"version\": \"${OLD_VERSION}\"/\"version\": \"${NEW_VERSION}\"/" tests/assertions.py
-sed -i '' -e "s/sentry.native\/${OLD_VERSION}\"/sentry.native\/${NEW_VERSION}\"/" tests/test_integration_http.py
+sed -E -i '' -e "s/\"version\": \"[^\"]+\"/\"version\": \"${NEW_VERSION}\"/" tests/assertions.py
+sed -E -i '' -e "s/sentry.native\/[^\"]+\"/sentry.native\/${NEW_VERSION}\"/" tests/test_integration_http.py
