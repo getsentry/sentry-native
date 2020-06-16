50fac7ea0151a805509bcce832f466d44bc57daa
Arpad Borsos <arpad.borsos@googlemail.com>
2020-06-16 18:27:05 +0200

    ci: Run analysis using CodeChecker

--- tests/cmake.py
+++ tests/cmake.py
@@ -1,4 +1,5 @@
 import os
+import json
 import sys
 import subprocess
 
@@ -47,9 +48,23 @@ def cmake(cwd, targets, options=None):
                 "ANDROID_NATIVE_API_LEVEL": os.environ["ANDROID_API"],
             }
         )
+
+    source_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
     cmake = ["cmake"]
     if "scan-build" in os.environ.get("RUN_ANALYZER", ""):
-        cmake = ["scan-build", "cmake"]
+        cc = os.environ.get("CC")
+        cxx = os.environ.get("CXX")
+        cmake = [
+            "scan-build",
+            *(["--use-cc", cc] if cc else []),
+            *(["--use-c++", cxx] if cxx else []),
+            "--status-bugs",
+            "--exclude",
+            os.path.join(source_dir, "external"),
+            "cmake",
+        ]
+
     configcmd = [*cmake]
     for key, value in options.items():
         configcmd.append("-D{}={}".format(key, value))
@@ -60,8 +75,7 @@ def cmake(cwd, targets, options=None):
     if "asan" in os.environ.get("RUN_ANALYZER", ""):
         configcmd.append("-DWITH_ASAN_OPTION=ON")
 
-    cmakelists_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    configcmd.append(cmakelists_dir)
+    configcmd.append(source_dir)
 
     # we have to set `-Werror` for this cmake invocation only, otherwise
     # completely unrelated things will break
@@ -81,14 +95,70 @@ def cmake(cwd, targets, options=None):
     except subprocess.CalledProcessError:
         pytest.fail("cmake configure failed")
 
+    # CodeChecker invocations and options are documented here:
+    # https://github.com/Ericsson/codechecker/blob/master/docs/analyzer/user_guide.md
+
     buildcmd = [*cmake, "--build", ".", "--parallel"]
     for target in targets:
         buildcmd.extend(["--target", target])
+    if "code-checker" in os.environ.get("RUN_ANALYZER", ""):
+        buildcmd = [
+            "CodeChecker",
+            "log",
+            "--build",
+            " ".join(buildcmd),
+            "--output",
+            "compilation.json",
+        ]
+    # if "code-checker" in os.environ.get("RUN_ANALYZER", ""):
+    #    buildcmd = ["CodeChecker", "check", "--ctu-all", "--build", " ".join(buildcmd)]
     print("{} > {}".format(cwd, " ".join(buildcmd)), flush=True)
     try:
         subprocess.run(buildcmd, cwd=cwd, check=True)
     except subprocess.CalledProcessError:
         pytest.fail("cmake build failed")
+
+    if "code-checker" in os.environ.get("RUN_ANALYZER", ""):
+        # For whatever reason, the compilation summary contains duplicate entries,
+        # one with the correct absolute path, and the other one just with the basename,
+        # which would fail.
+        with open(os.path.join(cwd, "compilation.json")) as f:
+            compilation = json.load(f)
+            compilation = list(filter(lambda c: c["file"].startswith("/"), compilation))
+        with open(os.path.join(cwd, "compilation.json"), "w") as f:
+            json.dump(compilation, f)
+
+        disable = [
+            "readability-magic-numbers",
+            "cppcoreguidelines-avoid-magic-numbers",
+            "readability-else-after-return",
+        ]
+        disables = []
+        for d in disable:
+            disables.append("--disable")
+            disables.append(d)
+        checkcmd = [
+            "CodeChecker",
+            "check",
+            "--jobs",
+            str(os.cpu_count()),
+            "--ctu-all",
+            # TODO: we currently get >300 reports with `enable-all`
+            # "--enable-all",
+            # *disables,
+            "--print-steps",
+            "--ignore",
+            os.path.join(source_dir, ".codechecker-ignore"),
+            "--logfile",
+            "compilation.json",
+        ]
+        child = subprocess.run(checkcmd, stdout=subprocess.PIPE, cwd=cwd, check=True)
+        sys.stdout.buffer.write(child.stdout)
+        marker = b"Total number of reports: "
+        errors = child.stdout[child.stdout.rfind(marker) + len(marker) :]
+        errors = int(errors[: errors.find(b"\n")])
+        if errors > 0:
+            pytest.fail("code-checker analysis failed")
 
     if os.environ.get("ANDROID_API"):
         # copy the output to the android image via adb
