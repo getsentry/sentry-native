679bf13a6854aa90a26c474b8e5693b491ea95d8
Javier Blazquez <jblazquez@riotgames.com>
2020-06-20 00:10:37 -0700

    crashpad: Enable Linux builds

--- tests/conditions.py
+++ tests/conditions.py
@@ -4,7 +4,7 @@ import os
 is_android = os.environ.get("ANDROID_API")
 is_x86 = os.environ.get("TEST_X86")
 
-has_crashpad = sys.platform != "linux" and not is_android
+has_crashpad = not is_android
 # 32-bit linux has no proper curl support
 has_http = not is_android and not is_x86
 has_inproc = True
