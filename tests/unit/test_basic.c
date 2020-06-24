6acb942189743d9ac5e9cace7ea141075c4c2d6a
Arpad Borsos <arpad.borsos@sentry.io>
2020-06-24 11:46:50 +0200

    test discarding events because of consent

--- tests/unit/test_basic.c
+++ tests/unit/test_basic.c
@@ -36,9 +36,14 @@ SENTRY_TEST(basic_function_transport)
     sentry_options_set_transport(
         options, sentry_new_function_transport(send_envelope, &called));
     sentry_options_set_release(options, "prod");
+    sentry_options_set_require_user_consent(options, true);
     sentry_init(options);
 
     sentry_set_transaction("demo-trans");
+
+    sentry_capture_event(sentry_value_new_message_event(
+        SENTRY_LEVEL_INFO, "root", "not captured due to missing consent"));
+    sentry_user_consent_give();
 
     sentry_capture_event(sentry_value_new_message_event(
         SENTRY_LEVEL_INFO, "root", "Hello World!"));
@@ -47,6 +52,10 @@ SENTRY_TEST(basic_function_transport)
     // something that is not a uuid, as this will be forcibly changed
     sentry_value_set_by_key(obj, "event_id", sentry_value_new_int32(1234));
     sentry_capture_event(obj);
+
+    sentry_user_consent_revoke();
+    sentry_capture_event(sentry_value_new_message_event(SENTRY_LEVEL_INFO,
+        "root", "not captured either due to revoked consent"));
 
     sentry_shutdown();
 
