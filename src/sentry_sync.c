089ede889660cd5e781ed57084a193a4123f69c6
irov <irov13@mail.ru>
2020-06-26 16:33:21 +0300

    fix MinGW32 only build

--- src/sentry_sync.c
+++ src/sentry_sync.c
@@ -58,7 +58,7 @@ shutdown_task(void *data)
 #    define THREAD_FUNCTION_API
 #endif
 
-#ifdef __MINGW32__
+#if defined(__MINGW32__) && !defined(__MINGW64__)
 #    define UNSIGNED_MINGW unsigned
 #else
 #    define UNSIGNED_MINGW
