458a2a1afdbf078b5bd35e4c43e16b456548b924
Arpad Borsos <arpad.borsos@sentry.io>
2020-06-26 15:13:03 +0200

    correct test conditions

--- tests/conditions.py
+++ tests/conditions.py
@@ -5,12 +5,12 @@ is_android = os.environ.get("ANDROID_API")
 is_x86 = os.environ.get("TEST_X86")
 is_kcov = "kcov" in os.environ.get("RUN_ANALYZER", "")
 
-# crashpad requires http
-has_crashpad = has_http and not is_android
 # 32-bit linux has no proper curl support
 has_http = not is_android and not is_x86
 has_inproc = True
 # breakpad does not work correctly when using kcov
 has_breakpad = not is_kcov and (sys.platform == "linux" or sys.platform == "win32")
+# crashpad requires http
+has_crashpad = has_http and not is_android
 # android has no local filesystem
 has_files = not is_android
