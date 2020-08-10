25b0a42812d380985ed630a46985d1339130beb7
Arpad Borsos <arpad.borsos@googlemail.com>
2020-08-10 12:24:32 +0200

    build unit tests without http

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -3,6 +3,8 @@ from . import run
 
 
 def test_unit(cmake, unittest):
-    cwd = cmake(["sentry_test_unit"], {"SENTRY_BACKEND": "none"})
+    cwd = cmake(
+        ["sentry_test_unit"], {"SENTRY_BACKEND": "none", "SENTRY_TRANSPORT": "none"}
+    )
     env = dict(os.environ)
     run(cwd, "sentry_test_unit", ["--no-summary", unittest], check=True, env=env)
