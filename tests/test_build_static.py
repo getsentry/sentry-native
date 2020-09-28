81dc547b0eb78acd31a98b1384fe80ae68d2847d
Arpad Borsos <arpad.borsos@googlemail.com>
2020-09-28 18:07:16 +0200

    check the PE file header directly on windows

--- tests/test_build_static.py
+++ tests/test_build_static.py
@@ -20,19 +20,19 @@ def test_static_lib(cmake):
         output = subprocess.check_output("ldd sentry_example", cwd=tmp_path, shell=True)
         assert b"libsentry.so" not in output
 
-    # on windows, we use `dumpbin` to check that the exe is compiled correctly
+    # on windows, we read the file header to check that the exe is compiled correctly
     if sys.platform == "win32":
-        output = subprocess.run(
-            "dumpbin /headers sentry_example.exe",
-            cwd=tmp_path,
-            shell=True,
-            stdout=subprocess.PIPE,
-        ).stdout
-        assert (
-            b"14C machine (x86)"
-            if os.environ.get("TEST_X86")
-            else b"8664 machine (x64)"
-        ) in output
+        with open(os.path.join(tmp_path, "sentry_example.exe"), "rb") as binary:
+            binary.seek(0x3C, 0)
+            offset = int.from_bytes(binary.read(4), byteorder="little")
+            binary.seek(offset, 0)
+            magic = binary.read(6)
+            # https://docs.microsoft.com/en-us/windows/win32/debug/pe-format#signature-image-only
+            assert magic == (
+                b"PE\x00\x00\x4c\x01"
+                if os.environ.get("TEST_X86")
+                else b"PE\x00\x00\x64\x86"
+            )
     # similarly, we use `file` on linux
     if sys.platform == "linux":
         output = subprocess.check_output(
