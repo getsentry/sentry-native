aa733d525b3b871c8f1d4b7f900d222f938f8b7e
Arpad Borsos <arpad.borsos@sentry.io>
2020-09-29 15:47:09 +0200

    ci: Switch to GitHub Actions (#383)

--- tests/test_build_static.py
+++ tests/test_build_static.py
@@ -20,15 +20,19 @@ def test_static_lib(cmake):
         output = subprocess.check_output("ldd sentry_example", cwd=tmp_path, shell=True)
         assert b"libsentry.so" not in output
 
-    # on windows, we use `sigcheck` to check that the exe is compiled correctly
+    # on windows, we read the file header to check that the exe is compiled correctly
     if sys.platform == "win32":
-        output = subprocess.run(
-            "sigcheck sentry_example.exe",
-            cwd=tmp_path,
-            shell=True,
-            stdout=subprocess.PIPE,
-        ).stdout
-        assert (b"32-bit" if os.environ.get("TEST_X86") else b"64-bit") in output
+        with open(os.path.join(tmp_path, "sentry_example.exe"), "rb") as binary:
+            binary.seek(0x3C, 0)
+            offset = int.from_bytes(binary.read(4), byteorder="little")
+            binary.seek(offset, 0)
+            magic = binary.read(6)
+            # https://docs.microsoft.com/en-us/windows/win32/debug/pe-format#signature-image-only
+            assert magic == (
+                b"PE\x00\x00\x4c\x01"
+                if os.environ.get("TEST_X86")
+                else b"PE\x00\x00\x64\x86"
+            )
     # similarly, we use `file` on linux
     if sys.platform == "linux":
         output = subprocess.check_output(
