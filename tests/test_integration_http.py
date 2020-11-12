04eb4e61f0227566bf0ba3da5db780cba85aff94
Arpad Borsos <arpad.borsos@sentry.io>
2020-11-12 14:05:05 +0100

    release: 0.4.4

--- tests/test_integration_http.py
+++ tests/test_integration_http.py
@@ -22,7 +22,7 @@ from .assertions import (
 pytestmark = pytest.mark.skipif(not has_http, reason="tests need http")
 
 auth_header = (
-    "Sentry sentry_key=uiaeosnrtdy, sentry_version=7, sentry_client=sentry.native/0.4.3"
+    "Sentry sentry_key=uiaeosnrtdy, sentry_version=7, sentry_client=sentry.native/0.4.4"
 )
 
 
