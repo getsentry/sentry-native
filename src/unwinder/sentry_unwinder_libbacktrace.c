76f1eee788b4c5ee5d608ceb69aa10f30143d279
Arpad Borsos <arpad.borsos@sentry.io>
2020-11-24 13:10:41 +0100

    guard by macos

--- src/unwinder/sentry_unwinder_libbacktrace.c
+++ src/unwinder/sentry_unwinder_libbacktrace.c
@@ -10,7 +10,8 @@ sentry__unwind_stack_libbacktrace(
     void *addr, const sentry_ucontext_t *uctx, void **ptrs, size_t max_frames)
 {
     if (addr) {
-#if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_14
+#if defined(SENTRY_PLATFORM_MACOS)                                             \
+    && __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_14
         return backtrace_from_fp(addr, ptrs, max_frames);
 #endif
         return 0;
