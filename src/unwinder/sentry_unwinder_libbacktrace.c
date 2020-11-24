c6c9dded4c13c93aa9300688d2f6d6242efa6d3a
Arpad Borsos <arpad.borsos@sentry.io>
2020-11-24 12:41:18 +0100

    fix: Use correct logic to detect macOS target

--- src/unwinder/sentry_unwinder_libbacktrace.c
+++ src/unwinder/sentry_unwinder_libbacktrace.c
@@ -1,13 +1,16 @@
 #include "sentry_boot.h"
 
 #include <execinfo.h>
+#ifdef SENTRY_PLATFORM_MACOS
+#    include <Availability.h>
+#endif
 
 size_t
 sentry__unwind_stack_libbacktrace(
     void *addr, const sentry_ucontext_t *uctx, void **ptrs, size_t max_frames)
 {
     if (addr) {
-#ifdef MAC_OS_X_VERSION_10_14
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_14
         return backtrace_from_fp(addr, ptrs, max_frames);
 #endif
         return 0;
--- tests/unit/test_unwinder.c
+++ tests/unit/test_unwinder.c
@@ -4,19 +4,16 @@
 
 #define MAX_FRAMES 128
 
-TEST_VISIBLE void **
-invoke_unwinder(void)
+TEST_VISIBLE size_t
+invoke_unwinder(void **backtrace)
 {
-    void **backtrace = sentry_malloc(sizeof(void *) * MAX_FRAMES);
-    memset(backtrace, 0, sizeof(void *) * MAX_FRAMES);
-
     // capture a stacktrace from within this function, which means that the
     // trace will include a frame of this very function that we will test
     // against.
     size_t frame_count = sentry_unwind_stack(NULL, backtrace, MAX_FRAMES);
+    // this assertion here makes sure the function is not inlined.
     TEST_CHECK(frame_count > 0);
-
-    return backtrace;
+    return frame_count;
 }
 
 static void
@@ -31,15 +28,15 @@ find_frame(const sentry_frame_info_t *info, void *data)
 
 SENTRY_TEST(unwinder)
 {
-    void **backtrace = invoke_unwinder();
+    void *backtrace[MAX_FRAMES] = { 0 };
+    size_t frame_count = invoke_unwinder(backtrace);
 
     int found_frame = 0;
-    for (int i = 0; i < MAX_FRAMES && backtrace[i]; i++) {
+    for (int i = 0; i < frame_count; i++) {
         // symbolize the frame, which also resolves an arbitrary instruction
         // address back to a function, and check if we found our invoker
         sentry__symbolize(backtrace[i], find_frame, &found_frame);
     }
-    sentry_free(backtrace);
 
     TEST_CHECK_INT_EQUAL(found_frame, 1);
 }
