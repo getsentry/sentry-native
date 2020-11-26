ec9c6dc357f7463bde87b13d900263f0a42aadb8
Arpad Borsos <arpad.borsos@sentry.io>
2020-11-26 13:52:30 +0100

    better builtin check

--- src/unwinder/sentry_unwinder_libbacktrace.c
+++ src/unwinder/sentry_unwinder_libbacktrace.c
@@ -1,8 +1,9 @@
 #include "sentry_boot.h"
 
 #include <execinfo.h>
-#ifdef SENTRY_PLATFORM_MACOS
-#    include <Availability.h>
+
+#ifndef __has_builtin
+#    define __has_builtin(x) 0
 #endif
 
 size_t
@@ -10,8 +11,7 @@ sentry__unwind_stack_libbacktrace(
     void *addr, const sentry_ucontext_t *uctx, void **ptrs, size_t max_frames)
 {
     if (addr) {
-#if defined(SENTRY_PLATFORM_MACOS) && defined(__clang__)                       \
-    && __has_builtin(__builtin_available)
+#if defined(SENTRY_PLATFORM_MACOS) && __has_builtin(__builtin_available)
         if (__builtin_available(macOS 10.14, *))
             return backtrace_from_fp(addr, ptrs, max_frames);
 #endif
