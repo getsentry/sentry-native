d9b0fd0596b38acdc0122443bb30a25cc00283f3
Arpad Borsos <arpad.borsos@sentry.io>
2020-11-26 12:42:08 +0100

    use builtin_available, and test unwinding from instruction pointer

--- src/unwinder/sentry_unwinder_libbacktrace.c
+++ src/unwinder/sentry_unwinder_libbacktrace.c
@@ -10,8 +10,10 @@ sentry__unwind_stack_libbacktrace(
     void *addr, const sentry_ucontext_t *uctx, void **ptrs, size_t max_frames)
 {
     if (addr) {
-#if defined(SENTRY_PLATFORM_MACOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101400
-        return backtrace_from_fp(addr, ptrs, max_frames);
+#if defined(SENTRY_PLATFORM_MACOS) && defined(__clang__)                       \
+    && __has_builtin(__builtin_available)
+        if (__builtin_available(macOS 10.14, *))
+            return backtrace_from_fp(addr, ptrs, max_frames);
 #endif
         return 0;
     } else if (uctx) {
--- tests/unit/test_unwinder.c
+++ tests/unit/test_unwinder.c
@@ -28,15 +28,40 @@ find_frame(const sentry_frame_info_t *info, void *data)
 
 SENTRY_TEST(unwinder)
 {
-    void *backtrace[MAX_FRAMES] = { 0 };
-    size_t frame_count = invoke_unwinder(backtrace);
+    void *backtrace1[MAX_FRAMES] = { 0 };
+    size_t frame_count1 = invoke_unwinder(backtrace1);
+    size_t invoker_frame = 0;
 
     int found_frame = 0;
-    for (size_t i = 0; i < frame_count; i++) {
+    for (size_t i = 0; i < frame_count1; i++) {
         // symbolize the frame, which also resolves an arbitrary instruction
         // address back to a function, and check if we found our invoker
-        sentry__symbolize(backtrace[i], find_frame, &found_frame);
+        sentry__symbolize(backtrace1[i], find_frame, &found_frame);
+        if (found_frame) {
+            invoker_frame = i;
+        }
     }
 
     TEST_CHECK_INT_EQUAL(found_frame, 1);
+
+    // the backtrace should have:
+    // X. something internal to sentry and the unwinder
+    // 2. the `invoke_unwinder` function
+    // 3. this test function here
+    // 4. whatever parent called that test function, which should have a stable
+    //    instruction pointer as long as we don’t return from here.
+    size_t offset = invoker_frame + 2;
+    if (frame_count1 >= offset) {
+        void *backtrace2[MAX_FRAMES] = { 0 };
+        size_t frame_count2
+            = sentry_unwind_stack(backtrace1[offset], backtrace2, MAX_FRAMES);
+
+        // we only have this functionality on some platforms / unwinders
+        if (frame_count2 > 0) {
+            TEST_CHECK_INT_EQUAL(frame_count2, frame_count1 - offset);
+            for (size_t i = 0; i < frame_count2; i++) {
+                TEST_CHECK_INT_EQUAL(backtrace2[i], backtrace1[offset + i]);
+            }
+        }
+    }
 }
