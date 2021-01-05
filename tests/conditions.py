989782943a5b42ad938760654cc7b7a1040e2839
Arpad Borsos <arpad.borsos@sentry.io>
2021-01-05 13:17:36 +0100

    feat: breakpad support for OSX (#448)
    Co-authored-by: Chris Rudd <rudd.chris.m@gmail.com>

--- tests/conditions.py
+++ tests/conditions.py
@@ -9,10 +9,13 @@ is_valgrind = "valgrind" in os.environ.get("RUN_ANALYZER", "")
 
 has_http = not is_android and not (sys.platform == "linux" and is_x86)
 # breakpad does not work correctly when using kcov or valgrind
+# also, asan reports a `stack-buffer-underflow` in breakpad itself,
+# and says this may be a false positive due to a custom stack unwinding mechanism
 has_breakpad = (
     not is_valgrind
     and not is_kcov
-    and (sys.platform == "linux" or sys.platform == "win32")
+    and not is_android
+    and not (is_asan and sys.platform == "darwin")
 )
 # crashpad requires http, and doesn’t work with kcov/valgrind either
 has_crashpad = has_http and not is_valgrind and not is_kcov and not is_android
