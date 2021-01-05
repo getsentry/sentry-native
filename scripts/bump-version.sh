57866be0508c7dc7850dceb4bbbcf29362730318
Jan Michael Auer <mail@jauer.org>
2021-01-05 16:58:56 +0100

    ci(gha): Switch to release workflow (#451)
    Switches the release process to the centralized release action. Craft is
    run directly on the host.

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 set -eux
 
+if [ "$(uname -s)" != "Linux" ]; then
+    echo "Please use the GitHub Action."
+    exit 1
+fi
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/..
 
-if [[ "$#" -eq 1 ]]; then
-  NEW_VERSION="$1"
-elif [[ "$#" -eq 2 ]]; then
-  OLD_VERSION="$1"
-  NEW_VERSION="$2"
-else
-  echo "Invalid number of arguments"
-  exit 1
-fi
+OLD_VERSION="$1"
+NEW_VERSION="$2"
 
-echo "Current version: ${OLD_VERSION}"
 echo "Bumping version: ${NEW_VERSION}"
 
-sed -i '' -e "s/^#define SENTRY_SDK_VERSION.*/#define SENTRY_SDK_VERSION \"${NEW_VERSION}\"/" include/sentry.h
-sed -E -i '' -e "s/\"version\": \"[^\"]+\"/\"version\": \"${NEW_VERSION}\"/" tests/assertions.py
-sed -E -i '' -e "s/sentry.native\/[^\"]+\"/sentry.native\/${NEW_VERSION}\"/" tests/test_integration_http.py
+perl -pi -e "s/^#define SENTRY_SDK_VERSION.*/#define SENTRY_SDK_VERSION \"${NEW_VERSION}\"/" include/sentry.h
+perl -pi -e "s/\"version\": \"[^\"]+\"/\"version\": \"${NEW_VERSION}\"/" tests/assertions.py
+perl -pi -e "s/sentry.native\/[^\"]+\"/sentry.native\/${NEW_VERSION}\"/" tests/test_integration_http.py
