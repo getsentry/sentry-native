ed7f98fa2b2cd1309535d0f40898ed853f7e19ea
Arpad Borsos <arpad.borsos@sentry.io>
2021-06-18 12:19:27 +0200

    fix: Avoid recursion when using `sentry_reinstall_backend` (#548)
    Previously, the `inproc` and `crashpad` (on linux) backends didn’t correctly reset their signal handlers when doing `reinstall_backend` (or multiple `init` calls for that matter).
    This has led to an infinite loop generating crashes.
    
    The fix now correctly unregisters the inproc/crashpad signal handlers, and adds an integration test using `reinstall_backend` to make sure we do not end up in an infinite loop.
    
    Co-authored-by: Mischa Alff <mischa@mutate.se>

--- src/backends/sentry_backend_crashpad.cpp
+++ src/backends/sentry_backend_crashpad.cpp
@@ -44,6 +44,27 @@ extern "C" {
 #ifdef SENTRY_PLATFORM_LINUX
 #    define SIGNAL_STACK_SIZE 65536
 static stack_t g_signal_stack;
+
+#    include "util/posix/signals.h"
+
+// This list was taken from crashpad's util/posix/signals.cc file
+// and is used to know which signals we need to reset to default
+// when shutting down the backend
+constexpr int g_CrashSignals[] = {
+    SIGABRT,
+    SIGBUS,
+    SIGFPE,
+    SIGILL,
+    SIGQUIT,
+    SIGSEGV,
+    SIGSYS,
+    SIGTRAP,
+#    if defined(SIGEMT)
+    SIGEMT,
+#    endif // defined(SIGEMT)
+    SIGXCPU,
+    SIGXFSZ,
+};
 #endif
 
 typedef struct {
@@ -281,6 +302,15 @@ sentry__crashpad_backend_startup(
 static void
 sentry__crashpad_backend_shutdown(sentry_backend_t *backend)
 {
+#ifdef SENTRY_PLATFORM_LINUX
+    // restore signal handlers to their default state
+    for (const auto signal : g_CrashSignals) {
+        if (crashpad::Signals::IsCrashSignal(signal)) {
+            crashpad::Signals::InstallDefaultHandler(signal);
+        }
+    }
+#endif
+
     crashpad_state_t *data = (crashpad_state_t *)backend->data;
     delete data->db;
     data->db = nullptr;
