fd0e0386779e9da12ff3160682c2fdf42cbac923
Betty Da <bda@sentry.io>
2021-12-09 01:58:03 -0500

    feat(tracing): Groundwork to add tracing context to all events

---
+++ src/sentry_tracing.c
@@ -0,0 +1,31 @@
+#include "sentry_sync.h"
+
+sentry_value_t
+sentry__span_get_trace_context(sentry_value_t span)
+{
+    if (sentry_value_is_null(span)
+        || sentry_value_is_null(sentry_value_get_by_key(span, "trace_id"))
+        || sentry_value_is_null(sentry_value_get_by_key(span, "span_id"))) {
+        return sentry_value_new_null();
+    }
+
+    sentry_value_t trace_context = sentry_value_new_object();
+
+#define PLACE_VALUE(Key, Source)                                               \
+    do {                                                                       \
+        sentry_value_t src = sentry_value_get_by_key(Source, Key);             \
+        if (!sentry_value_is_null(src)) {                                      \
+            sentry_value_incref(src);                                          \
+            sentry_value_set_by_key(trace_context, Key, src);                  \
+        }                                                                      \
+    } while (0)
+
+    PLACE_VALUE("trace_id", span);
+    PLACE_VALUE("span_id", span);
+    PLACE_VALUE("parent_span_id", span);
+    PLACE_VALUE("op", span);
+    PLACE_VALUE("description", span);
+    PLACE_VALUE("status", span);
+
+    return trace_context;
+}
---
+++ src/sentry_tracing.h
@@ -0,0 +1,14 @@
+#ifndef SENTRY_TRACING_H_INCLUDED
+#define SENTRY_TRACING_H_INCLUDED
+
+#include "sentry_boot.h"
+#include "sentry_value.h"
+
+/**
+ * Returns an object containing tracing information extracted from a
+ * transaction (/span) which should be included in an event.
+ * See https://develop.sentry.dev/sdk/event-payloads/transaction/#examples
+ */
+sentry_value_t sentry__span_get_trace_context(sentry_value_t span);
+
+#endif
