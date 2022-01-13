c01555ca6e14db00a2b353ff8944bd2d3398309e
relaxolotl <5597345+relaxolotl@users.noreply.github.com>
2022-01-13 00:27:35 -0500

    feat(tracing): Introduce structs for performance monitoring constructs (#649)

--- examples/example.c
+++ examples/example.c
@@ -220,19 +220,19 @@ main(int argc, char **argv)
 
 #ifdef SENTRY_PERFORMANCE_MONITORING
     if (has_arg(argc, argv, "capture-transaction")) {
-        sentry_value_t tx_ctx
-            = sentry_value_new_transaction_context("little.teapot",
+        sentry_transaction_context_t *tx_ctx
+            = sentry_transaction_context_new("little.teapot",
                 "Short and stout here is my handle and here is my spout");
 
         if (has_arg(argc, argv, "unsample-tx")) {
             sentry_transaction_context_set_sampled(tx_ctx, 0);
         }
-        sentry_value_t tx = sentry_transaction_start(tx_ctx);
+        sentry_transaction_t *tx = sentry_transaction_start(tx_ctx);
 
         if (has_arg(argc, argv, "child-spans")) {
-            sentry_value_t child_ctx
-                = sentry_span_start_child(tx, "littler.teapot", NULL);
-            sentry_value_t grandchild_ctx
+            sentry_span_t *child_ctx
+                = sentry_transaction_start_child(tx, "littler.teapot", NULL);
+            sentry_span_t *grandchild_ctx
                 = sentry_span_start_child(child_ctx, "littlest.teapot", NULL);
 
             sentry_span_finish(tx, grandchild_ctx);
