6034fd00563b9440c24ebd27e3a340e9fd0beab6
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2022-08-06 15:30:09 +0200

    Implement parameterized crash tests (including WER)

--- tests/test_integration_crashpad.py
+++ tests/test_integration_crashpad.py
@@ -8,7 +8,6 @@ from .assertions import assert_crashpad_upload, assert_session
 
 pytestmark = pytest.mark.skipif(not has_crashpad, reason="tests need crashpad backend")
 
-
 # Windows and Linux are currently able to flush all the state on crash
 flushes_state = sys.platform != "darwin"
 
@@ -46,7 +45,88 @@ def test_crashpad_reinstall(cmake, httpserver):
     assert len(httpserver.log) == 1
 
 
-def run_crashpad_dumping_crash(cmake, httpserver, custom_args):
+@pytest.mark.skipif(
+    sys.platform != "win32",
+    reason="Test covers Windows-specific crashes which can only be covered via the Crashpad WER module",
+)
+@pytest.mark.parametrize(
+    "run_args",
+    [
+        # discarding via before-send or on-crash has no consequence for fast-fail crashes because they by-pass SEH
+        (["stack-buffer-overrun"]),
+        (["stack-buffer-overrun", "discarding-before-send"]),
+        (["stack-buffer-overrun", "discarding-on-crash"]),
+        (["fastfail"]),
+        (["fastfail", "discarding-before-send"]),
+        (["fastfail", "discarding-on-crash"]),
+    ],
+)
+def test_crashpad_wer_crash(cmake, httpserver, run_args):
+    tmp_path = cmake(["sentry_example"], {"SENTRY_BACKEND": "crashpad"})
+
+    # If we are building on a Windows without WER enabled this test doesn't make sense
+    if not os.path.exists(tmp_path / "crashpad_wer.dll"):
+        return
+
+    env = dict(os.environ, SENTRY_DSN=make_dsn(httpserver))
+    httpserver.expect_oneshot_request("/api/123456/minidump/").respond_with_data("OK")
+    httpserver.expect_request("/api/123456/envelope/").respond_with_data("OK")
+
+    with httpserver.wait(timeout=10) as waiting:
+        child = run(
+            tmp_path,
+            "sentry_example",
+            ["log", "start-session", "attachment", "overflow-breadcrumbs"] + run_args,
+            env=env,
+        )
+        assert child.returncode  # well, it's a crash after all
+
+    assert waiting.result
+
+    # the session crash heuristic on mac uses timestamps, so make sure we have
+    # a small delay here
+    time.sleep(1)
+
+    run(tmp_path, "sentry_example", ["log", "no-setup"], check=True, env=env)
+
+    assert len(httpserver.log) == 2
+    outputs = (httpserver.log[0][0], httpserver.log[1][0])
+    session, multipart = (
+        (outputs[0].get_data(), outputs[1])
+        if b'"type":"session"' in outputs[0].get_data()
+        else (outputs[1].get_data(), outputs[0])
+    )
+
+    envelope = Envelope.deserialize(session)
+
+    assert_session(envelope, {"status": "crashed", "errors": 1})
+    assert_crashpad_upload(multipart)
+
+
+@pytest.mark.parametrize(
+    "run_args",
+    [
+        # if we crash, we want a dump
+        ([]),
+        # if we crash and before-send doesn't discard, we want a dump
+        pytest.param(
+            ["before-send"],
+            marks=pytest.mark.skipif(
+                sys.platform == "darwin",
+                reason="crashpad doesn't provide SetFirstChanceExceptionHandler on macOS",
+            ),
+        ),
+        # if on_crash() is non-discarding, a discarding before_send() is overruled, so we get a dump
+        pytest.param(
+            ["discarding-before-send", "on-crash"],
+            marks=pytest.mark.skipif(
+                sys.platform == "darwin",
+                reason="crashpad doesn't provide SetFirstChanceExceptionHandler on macOS",
+            ),
+        ),
+    ],
+)
+def test_crashpad_dumping_crash(cmake, httpserver, run_args):
     tmp_path = cmake(["sentry_example"], {"SENTRY_BACKEND": "crashpad"})
 
     env = dict(os.environ, SENTRY_DSN=make_dsn(httpserver))
@@ -58,7 +138,7 @@ def run_crashpad_dumping_crash(cmake, httpserver, custom_args):
             tmp_path,
             "sentry_example",
             ["log", "start-session", "attachment", "overflow-breadcrumbs", "crash"]
-            + custom_args,
+            + run_args,
             env=env,
         )
         assert child.returncode  # well, its a crash after all
@@ -79,10 +159,21 @@ def run_crashpad_dumping_crash(cmake, httpserver, custom_args):
         else (outputs[1].get_data(), outputs[0])
     )
 
-    return session, multipart
+    envelope = Envelope.deserialize(session)
+
+    assert_session(envelope, {"status": "crashed", "errors": 1})
+    assert_crashpad_upload(multipart)
 
 
-def run_crashpad_non_dumping_crash(cmake, httpserver, custom_args):
+@pytest.mark.skipif(
+    sys.platform == "darwin",
+    reason="crashpad doesn't provide SetFirstChanceExceptionHandler on macOS",
+)
+@pytest.mark.parametrize(
+    "run_args",
+    [(["discarding-before-send"]), (["discarding-on-crash"])],
+)
+def test_crashpad_non_dumping_crash(cmake, httpserver, run_args):
     tmp_path = cmake(["sentry_example"], {"SENTRY_BACKEND": "crashpad"})
 
     env = dict(os.environ, SENTRY_DSN=make_dsn(httpserver))
@@ -99,10 +190,10 @@ def run_crashpad_non_dumping_crash(cmake, httpserver, custom_args):
                 "overflow-breadcrumbs",
                 "crash",
             ]
-            + custom_args,
+            + run_args,
             env=env,
         )
-        assert child.returncode  # well, its a crash after all
+        assert child.returncode  # well, it's a crash after all
 
     assert waiting.result is False
 
@@ -115,77 +206,9 @@ def run_crashpad_non_dumping_crash(cmake, httpserver, custom_args):
     assert len(httpserver.log) == 1
     output = httpserver.log[0][0]
     session = output.get_data()
-
-    return session
-
-
-def test_crashpad_crash(cmake, httpserver):
-    session, multipart = run_crashpad_dumping_crash(cmake, httpserver, [])
-
-    envelope = Envelope.deserialize(session)
-
-    assert_session(envelope, {"status": "crashed", "errors": 1})
-    assert_crashpad_upload(multipart)
-
-
-@pytest.mark.skipif(
-    sys.platform == "darwin",
-    reason="crashpad doesn't provide SetFirstChanceExceptionHandler on macOS",
-)
-def test_crashpad_crash_before_send(cmake, httpserver):
-    session, multipart = run_crashpad_dumping_crash(cmake, httpserver, ["before-send"])
-
-    envelope = Envelope.deserialize(session)
-
-    assert_session(envelope, {"status": "crashed", "errors": 1})
-    assert_crashpad_upload(multipart)
-
-    # TODO(supervacuus): we would expect to see a change coming from the
-    # before_send() hook, but in contrast to the other backends the crashpad
-    # backend currently only checks for null-value as a signal not to produce
-    # a minidump and after this decrefs the event.
-
-
-@pytest.mark.skipif(
-    sys.platform == "darwin",
-    reason="crashpad doesn't provide SetFirstChanceExceptionHandler on macOS",
-)
-def test_crashpad_crash_discarding_before_send(cmake, httpserver):
-    session = run_crashpad_non_dumping_crash(
-        cmake, httpserver, ["discarding-before-send"]
-    )
-
     envelope = Envelope.deserialize(session)
 
     assert_session(envelope, {"status": "abnormal", "errors": 0})
-
-
-@pytest.mark.skipif(
-    sys.platform == "darwin",
-    reason="crashpad doesn't provide SetFirstChanceExceptionHandler on macOS",
-)
-def test_crashpad_crash_discarding_on_crash(cmake, httpserver):
-    session = run_crashpad_non_dumping_crash(cmake, httpserver, ["discarding-on-crash"])
-
-    envelope = Envelope.deserialize(session)
-
-    assert_session(envelope, {"status": "abnormal", "errors": 0})
-
-
-@pytest.mark.skipif(
-    sys.platform == "darwin",
-    reason="crashpad doesn't provide SetFirstChanceExceptionHandler on macOS",
-)
-def test_crashpad_crash_discarding_before_send_and_on_crash(cmake, httpserver):
-    session, multipart = run_crashpad_dumping_crash(
-        cmake, httpserver, ["discarding-before-send", "on-crash"]
-    )
-
-    envelope = Envelope.deserialize(session)
-
-    # on_crash() is defined and overrules the discarding before_send()
-    assert_session(envelope, {"status": "crashed", "errors": 1})
-    assert_crashpad_upload(multipart)
 
 
 @pytest.mark.skipif(
