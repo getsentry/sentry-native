2afe919271af8198de56d5e34b03ca8a423cf384
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2022-10-03 17:18:23 +0200

    The terminal commit of the direct libunwindstack loader

--- src/unwinder/sentry_unwinder_libunwindstack.cpp
+++ src/unwinder/sentry_unwinder_libunwindstack.cpp
@@ -38,54 +38,6 @@ struct FrameData {
     int map_flags = 0;
 };
 
-struct MapInfo {
-    MapInfo(MapInfo *prev_map, MapInfo *prev_real_map, uint64_t start,
-        uint64_t end, uint64_t offset, uint64_t flags, const char *name);
-
-    MapInfo(MapInfo *prev_map, MapInfo *prev_real_map, uint64_t start,
-        uint64_t end, uint64_t offset, uint64_t flags, const std::string &name);
-
-    virtual ~MapInfo();
-};
-
-class Maps {
-public:
-    virtual ~Maps();
-
-    Maps();
-
-    Maps(const Maps &);
-
-    Maps &operator=(const Maps &);
-
-    Maps(Maps &&);
-
-    Maps &operator=(Maps &&);
-
-    virtual bool Parse();
-};
-
-class RemoteMaps : public Maps {
-public:
-    RemoteMaps(pid_t pid);
-
-    virtual ~RemoteMaps();
-};
-
-class LocalMaps : public RemoteMaps {
-public:
-    LocalMaps()
-        : RemoteMaps(getpid())
-    {
-    }
-
-    virtual ~LocalMaps() = default;
-};
-
-struct Regs {
-    virtual uint64_t pc() = 0;
-};
-
 enum ArchEnum : uint8_t {
     ARCH_UNKNOWN = 0,
     ARCH_ARM,
@@ -95,41 +47,87 @@ enum ArchEnum : uint8_t {
 };
 
 class Memory;
-
-class Unwinder {
-public:
-    Unwinder(size_t max_frames, Maps *maps, Regs *regs,
-        std::shared_ptr<Memory> process_memory);
-
-    virtual ~Unwinder();
-
-    virtual void Unwind(
-        const std::vector<std::string> *initial_map_names_to_skip,
-        const std::vector<std::string> *map_suffixes_to_ignore)
-        = 0;
-
-    virtual std::vector<FrameData> &frames() = 0;
+enum ErrorCode : uint8_t {
+    ERROR_NONE, // No error.
+    ERROR_MEMORY_INVALID, // Memory read failed.
+    ERROR_UNWIND_INFO, // Unable to use unwind information to unwind.
+    ERROR_UNSUPPORTED, // Encountered unsupported feature.
+    ERROR_INVALID_MAP, // Unwind in an invalid map.
+    ERROR_MAX_FRAMES_EXCEEDED, // The number of frames exceed the total allowed.
+    ERROR_REPEATED_FRAME, // The last frame has the same pc/sp as the next.
+    ERROR_INVALID_ELF, // Unwind in an invalid elf.
+    ERROR_THREAD_DOES_NOT_EXIST, // Attempt to unwind a local thread that does
+    // not exist.
+    ERROR_THREAD_TIMEOUT, // Timeout trying to unwind a local thread.
+    ERROR_SYSTEM_CALL, // System call failed while unwinding.
+};
+struct ErrorData {
+    ErrorCode code;
+    uint64_t address; // Only valid when code is ERROR_MEMORY_INVALID.
+    // Indicates the failing address.
 };
 
-typedef Regs *(*create_from_ucontext_t)(ArchEnum arch, void *ucontext);
+typedef void *(*create_from_ucontext_t)(ArchEnum arch, void *ucontext);
 
 typedef ArchEnum (*current_arch_t)();
 
-typedef Regs *(*create_from_local_t)();
-
+typedef void *(*create_from_local_t)();
+typedef void *(*local_maps_ctor_t)(void *);
+typedef bool (*maps_parse_t)(void *);
+typedef void *(*regs_pc_t)(void *);
+typedef void *(*unwinder_ctor_t)(void *);
+typedef void (*unwinder_unwind_t)(void *, void *, void *);
+typedef std::vector<FrameData> &(*unwinder_frames_t)(void *);
 typedef std::shared_ptr<Memory> (*create_process_memory_cached_t)(pid_t);
 
-static create_from_ucontext_t create_from_ucontext;
-static current_arch_t current_arch;
-static create_from_local_t create_from_local;
-static create_process_memory_cached_t create_process_memory_cached;
-static void *handle;
+static create_from_ucontext_t create_from_ucontext = nullptr;
+static current_arch_t current_arch = nullptr;
+static create_from_local_t create_from_local = nullptr;
+static create_process_memory_cached_t create_process_memory_cached = nullptr;
+static local_maps_ctor_t local_maps_ctor = nullptr;
+static maps_parse_t maps_parse = nullptr;
+static regs_pc_t regs_pc = nullptr;
+static unwinder_ctor_t unwinder_ctor = nullptr;
+static unwinder_unwind_t unwinder_unwind = nullptr;
+static unwinder_frames_t unwinder_frames = nullptr;
+static void *handle = nullptr;
+
+struct unwinder_data {
+    unwinder_data(size_t max_frames, void *maps, void *regs,
+        std::shared_ptr<Memory> process_memory)
+        : max_frames_(max_frames)
+        , maps_(maps)
+        , regs_(regs)
+        , process_memory_(process_memory)
+        , arch_(current_arch())
+    {
+    }
+
+    size_t max_frames_;
+    void *maps_;
+    void *regs_;
+    std::vector<FrameData> frames_;
+    std::shared_ptr<Memory> process_memory_;
+    void *jit_debug_ = nullptr;
+    void *dex_files_ = nullptr;
+    bool resolve_names_ = true;
+    bool embedded_soname_ = true;
+    bool display_build_id_ = false;
+    // True if at least one elf file is coming from memory and not the related
+    // file. This is only true if there is an actual file backing up the elf.
+    bool elf_from_memory_not_file_ = false;
+    ErrorData last_error_ = { ERROR_NONE, 0 };
+    uint64_t warnings_ = 0;
+    ArchEnum arch_ = ARCH_UNKNOWN;
+};
 
 static bool
 is_unwinder_loaded()
 {
-    return create_from_ucontext != nullptr && create_from_local != nullptr
-        && current_arch != nullptr && create_process_memory_cached != nullptr;
+    return create_from_ucontext && current_arch && create_from_local
+        && create_process_memory_cached && local_maps_ctor && maps_parse
+        && regs_pc && unwinder_ctor && unwinder_unwind && unwinder_frames
+        && handle;
 }
 
 static int
@@ -155,6 +153,39 @@ load_unwinder_symbols()
     if (!create_process_memory_cached) {
         return EXIT_FAILURE;
     }
+
+    local_maps_ctor
+        = (local_maps_ctor_t)dlsym(handle, "_ZTVN11unwindstack9LocalMapsE");
+    if (!local_maps_ctor) {
+        return EXIT_FAILURE;
+    }
+
+    maps_parse = (maps_parse_t)dlsym(handle, "_ZN11unwindstack4Maps5ParseEv");
+    if (!maps_parse) {
+        return EXIT_FAILURE;
+    }
+
+#if defined(__arm__)
+    regs_pc = (regs_pc_t)dlsym(handle, "_ZN11unwindstack7RegsArm2pcEv");
+#elif defined(__aarch64__)
+    regs_pc = (regs_pc_t)dlsym(handle, "_ZN11unwindstack9RegsArm642pcEv");
+#elif defined(__i386__)
+    regs_pc = (regs_pc_t)dlsym(handle, "_ZN11unwindstack7RegsX862pcEv");
+#elif defined(__x86_64__)
+    regs_pc = (regs_pc_t)dlsym(handle, "_ZN11unwindstack10RegsX86_642pcEv");
+#endif
+    if (!regs_pc) {
+        return EXIT_FAILURE;
+    }
+
+    unwinder_unwind = (unwinder_unwind_t)dlsym(handle,
+        "_ZN11unwindstack8Unwinder6UnwindEPKNSt3__16vectorINS1_12basic_"
+        "stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEENS6_IS8_EEEESC_");
+    if (!unwinder_unwind) {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
 
 extern "C" {
@@ -163,27 +194,40 @@ sentry__unwind_stack_libunwindstack(
     void *addr, const sentry_ucontext_t *uctx, void **ptrs, size_t max_frames)
 {
     if (is_unwinder_loaded()) {
-        Regs *regs = create_from_ucontext(current_arch(), uctx->user_context);
-        LocalMaps maps;
-        if (!maps.Parse()) {
-            ptrs[0] = (void *)regs->pc();
+        SENTRY_DEBUG("create_from_ucontext");
+
+        void *regs = create_from_ucontext(current_arch(), uctx->user_context);
+        SENTRY_DEBUG("allocate maps data");
+        void *maps_data = malloc(32); // std::vector + pid_t
+        SENTRY_DEBUG("construct LocalMaps");
+        local_maps_ctor(maps_data);
+        SENTRY_DEBUG("parse LocalMaps");
+        if (!maps_parse(maps_data)) {
+            SENTRY_DEBUG("retrieve PC from regs");
+            ptrs[0] = regs_pc(regs);
             return 1;
         }
 
+        SENTRY_DEBUG("create_process_memory_cached");
         const std::shared_ptr<Memory> process_memory
             = create_process_memory_cached(getpid());
 
-        Unwinder unwinder(max_frames, &maps, regs, process_memory);
-        unwinder.Unwind();
-
-        std::vector<FrameData> &frames = unwinder.frames();
+        SENTRY_DEBUG("construct unwinder");
+        auto *unwinder_data = new struct unwinder_data(
+            max_frames, maps_data, regs, process_memory);
+        SENTRY_DEBUG("unwind");
+        unwinder_unwind(unwinder_data, nullptr, nullptr);
 
         size_t rv = 0;
-        for (FrameData &frame : frames) {
+        SENTRY_DEBUG("iterate stack frames");
+        for (const FrameData &frame : unwinder_data->frames_) {
+            SENTRY_DEBUG("store frame pointer");
             ptrs[rv++] = (void *)frame.pc;
         }
 
-        delete regs;
+        delete unwinder_data;
+        free(maps_data);
+        free(regs);
 
         return rv;
     } else {
@@ -201,29 +245,32 @@ sentry__load_unwinder()
         char *end;
         sdk_ver = strtol(sdk_ver_str, &end, 10);
         if (sdk_ver_str == end) {
-            SENTRY_WARN(
+            SENTRY_ERROR(
                 "failed to read android SDK version to choose unwinder");
             return;
         }
     } else {
-        SENTRY_WARN(
+        SENTRY_ERROR(
             "failed to find android system property \"ro.build.version.sdk\"");
         return;
     }
 
     if (sdk_ver >= 28) {
         SENTRY_DEBUG("running on android >= 28; load OS unwinder");
-        handle = dlopen("libunwindstack.so", RTLD_LOCAL | RTLD_LAZY);
+        handle
+            = dlopen("/system/lib64/libunwindstack.so", RTLD_LOCAL | RTLD_LAZY);
     } else {
         SENTRY_DEBUG("running on android < 28; load legacy unwinder");
         handle = dlopen("libandroid_unwinder_legacy.so", RTLD_LOCAL | RTLD_NOW);
     }
     if (!handle) {
-        SENTRY_WARN("failed to load unwinder library");
+        SENTRY_ERROR("failed to load unwinder library");
+        SENTRY_ERROR(dlerror());
         return;
     }
     if (load_unwinder_symbols() == EXIT_FAILURE) {
-        SENTRY_WARN("failed to load unwinder symbols");
+        SENTRY_ERROR("failed to load unwinder symbols");
+        SENTRY_ERROR(dlerror());
         return;
     }
 }
