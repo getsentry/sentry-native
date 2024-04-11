b038b1f6fb41c490bb3b3f350792f9ea190fe757
Azat Khuzhin <a3at.mail@gmail.com>
2024-04-11 16:09:20 +0200

    Add ability to disable page allocator
    This is required to avoid leaks, since free() with page allocator is
    noop.

--- src/sentry_alloc.c
+++ src/sentry_alloc.c
@@ -5,15 +5,14 @@
 
 /* on unix platforms we add support for a simplistic page allocator that can
    be enabled to make code async safe */
-#ifdef SENTRY_PLATFORM_UNIX
+#ifdef SENTRY_ENABLE_PAGE_ALLOCATOR
 #    include "sentry_unix_pageallocator.h"
-#    define WITH_PAGE_ALLOCATOR
 #endif
 
 void *
 sentry_malloc(size_t size)
 {
-#ifdef WITH_PAGE_ALLOCATOR
+#ifdef SENTRY_ENABLE_PAGE_ALLOCATOR
     if (sentry__page_allocator_enabled()) {
         return sentry__page_allocator_alloc(size);
     }
@@ -24,7 +23,7 @@ sentry_malloc(size_t size)
 void
 sentry_free(void *ptr)
 {
-#ifdef WITH_PAGE_ALLOCATOR
+#ifdef SENTRY_ENABLE_PAGE_ALLOCATOR
     /* page allocator can't free */
     if (sentry__page_allocator_enabled()) {
         return;
