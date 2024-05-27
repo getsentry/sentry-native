15148d732591c79ef9d91f2694786343d32ed812
Ivan Tustanivskyi <tustanivsky@gmail.com>
2024-05-27 16:55:28 +0300

    Add convenience macro for metrics test checks

--- tests/unit/test_metrics.c
+++ tests/unit/test_metrics.c
@@ -1,78 +1,59 @@
 ﻿#include "sentry_metrics.h"
 #include "sentry_testsupport.h"
 
+#define TEST_CHECK_METRICS_SANITY(fn, input, expected)                         \
+    {                                                                          \
+        char *actual = fn(input);                                              \
+        TEST_CHECK_STRING_EQUAL(actual, expected);                             \
+        sentry_free(actual);                                                   \
+    }
+
 SENTRY_TEST(metrics_name_sanitize)
 {
-    char *name1 = sentry__metrics_sanitize_name("foo-bar");
-    char *name2 = sentry__metrics_sanitize_name("foo\\!,bar");
-    char *name3 = sentry__metrics_sanitize_name("fo%-bar");
-    // char *name4 = sentry__metrics_sanitize_name("fôo-bär");
-
-    TEST_CHECK_STRING_EQUAL(name1, "foo-bar");
-    TEST_CHECK_STRING_EQUAL(name2, "foo\\__bar");
-    TEST_CHECK_STRING_EQUAL(name3, "fo_-bar");
-    // TEST_CHECK_STRING_EQUAL(name4, "f_o-b_r");
-
-    sentry_free(name1);
-    sentry_free(name2);
-    sentry_free(name3);
+    TEST_CHECK_METRICS_SANITY(
+        sentry__metrics_sanitize_name, "foo-bar", "foo-bar");
+    TEST_CHECK_METRICS_SANITY(
+        sentry__metrics_sanitize_name, "foo\\!,bar", "foo\\__bar");
+    TEST_CHECK_METRICS_SANITY(
+        sentry__metrics_sanitize_name, "fo%-bar", "fo_-bar");
+    TEST_CHECK_METRICS_SANITY(
+        sentry__metrics_sanitize_name, "fôo-bär", "f_o-b_r");
 }
 
 SENTRY_TEST(metrics_unit_sanitize)
 {
-    char *unit = sentry__metrics_sanitize_unit("abcABC123_-./\\/%&abcABC12");
-
-    TEST_CHECK_STRING_EQUAL(unit, "abcABC123_abcABC12");
-
-    sentry_free(unit);
+    TEST_CHECK_METRICS_SANITY(sentry__metrics_sanitize_unit,
+        "abcABC123_-./\\/%&abcABC12", "abcABC123_abcABC12");
 }
 
 SENTRY_TEST(metrics_tag_key_sanitize)
 {
-    char *key = sentry__metrics_sanitize_tag_key("a/weird/tag-key/:\\$");
-
-    TEST_CHECK_STRING_EQUAL(key, "a/weird/tag-key/\\");
-
-    sentry_free(key);
+    TEST_CHECK_METRICS_SANITY(sentry__metrics_sanitize_unit,
+        "a/weird/tag-key/:\\$", "a/weird/tag-key/\\");
 }
 
 SENTRY_TEST(metrics_tag_value_sanitize)
 {
-    char *val1 = sentry__metrics_sanitize_tag_value("plain");
-    char *val2 = sentry__metrics_sanitize_tag_value("plain text");
-    char *val3 = sentry__metrics_sanitize_tag_value("plain%text");
-
-    TEST_CHECK_STRING_EQUAL(val1, "plain");
-    TEST_CHECK_STRING_EQUAL(val2, "plain text");
-    TEST_CHECK_STRING_EQUAL(val3, "plain%text");
-
-    sentry_free(val1);
-    sentry_free(val2);
-    sentry_free(val3);
+    TEST_CHECK_METRICS_SANITY(
+        sentry__metrics_sanitize_unit, "plaintext", "plaintext");
+    TEST_CHECK_METRICS_SANITY(
+        sentry__metrics_sanitize_unit, "plain text", "plain text");
+    TEST_CHECK_METRICS_SANITY(
+        sentry__metrics_sanitize_unit, "plain%text", "plain%text");
 
     // Escape sequences
-    char *val4 = sentry__metrics_sanitize_tag_value("plain \\ text");
-    char *val5 = sentry__metrics_sanitize_tag_value("plain,text");
-    char *val6 = sentry__metrics_sanitize_tag_value("plain|text");
-
-    TEST_CHECK_STRING_EQUAL(val4, "plain \\\\ text");
-    TEST_CHECK_STRING_EQUAL(val5, "plain\\u{2c}text");
-    TEST_CHECK_STRING_EQUAL(val6, "plain\\u{7c}text");
-
-    sentry_free(val4);
-    sentry_free(val5);
-    sentry_free(val6);
+    TEST_CHECK_METRICS_SANITY(
+        sentry__metrics_sanitize_unit, "plain \\ text", "plain \\\\ text");
+    TEST_CHECK_METRICS_SANITY(
+        sentry__metrics_sanitize_unit, "plain,text", "plain\\u{2c}text");
+    TEST_CHECK_METRICS_SANITY(
+        sentry__metrics_sanitize_unit, "plain|text", "plain\\u{7c}text");
 
     // Escapable control characters
-    char *val7 = sentry__metrics_sanitize_tag_value("plain\ntext");
-    char *val8 = sentry__metrics_sanitize_tag_value("plain\rtext");
-    char *val9 = sentry__metrics_sanitize_tag_value("plain\ttext");
-
-    TEST_CHECK_STRING_EQUAL(val7, "plain\\ntext");
-    TEST_CHECK_STRING_EQUAL(val8, "plain\\rtext");
-    TEST_CHECK_STRING_EQUAL(val9, "plain\\ttext");
-
-    sentry_free(val7);
-    sentry_free(val8);
-    sentry_free(val9);
+    TEST_CHECK_METRICS_SANITY(
+        sentry__metrics_sanitize_unit, "plain\ntext", "plain\\ntext");
+    TEST_CHECK_METRICS_SANITY(
+        sentry__metrics_sanitize_unit, "plain\rtext", "plain\\rtext");
+    TEST_CHECK_METRICS_SANITY(
+        sentry__metrics_sanitize_unit, "plain\ttext", "plain\\ttext");
 }
