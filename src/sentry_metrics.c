d2330d20be1ca7d7791cbb5fb8d9443d9e020b3c
Ivan Tustanivskyi <tustanivsky@gmail.com>
2024-05-29 14:28:20 +0300

    Test another fix

--- src/sentry_metrics.c
+++ src/sentry_metrics.c
@@ -495,8 +495,6 @@ sentry__metrics_aggregator_add(const sentry_metrics_aggregator_t *aggregator,
 
 #ifdef SENTRY_INTEGRATIONTEST
     sentry__metrics_aggregator_flush(aggregator, true);
-#elif SENTRY_UNITTEST
-    // do nothing
 #else
     bool is_owerweight = sentry__metrics_is_overweight(aggregator);
     if (is_owerweight || !g_is_flush_scheduled) {
