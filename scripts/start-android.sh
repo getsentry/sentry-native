708eff7dc676fc11878231ebfea910f44e2e18ae
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2024-11-05 11:53:47 +0100

    accept android image licenses automatically

--- scripts/start-android.sh
+++ scripts/start-android.sh
@@ -25,7 +25,7 @@ echo "AVD_EMULATOR_NAME = $AVD_EMULATOR_NAME"
 echo "IMAGE = $IMAGE"
 
 echo "Install image..."
-$ANDROID_SDK_ROOT/cmdline-tools/latest/bin/sdkmanager --install "$IMAGE" 
+$ANDROID_SDK_ROOT/cmdline-tools/latest/bin/sdkmanager --install "$IMAGE" --licenses
 
 echo "Create AVD..."
 echo "no" | $ANDROID_HOME/cmdline-tools/latest/bin/avdmanager create avd -n $AVD_EMULATOR_NAME -k "$IMAGE" --force
