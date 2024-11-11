6e64ba3a5ebc88e1398ac3be9c0db43400f8dff2
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2024-11-11 17:42:22 +0100

    replace `TARGET` parameter with `TGT` for good measure

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,7 +1,7 @@
-# Generates a version resource file from the `sentry.rc.in` template for the `TARGET` argument and adds it as a source.
-function(sentry_add_version_resource TARGET FILE_DESCRIPTION)
+# Generates a version resource file from the `sentry.rc.in` template for the `TGT` argument and adds it as a source.
+function(sentry_add_version_resource TGT FILE_DESCRIPTION)
     # generate a resource output-path from the target name
-    set(RESOURCE_PATH "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.rc")
+    set(RESOURCE_PATH "${CMAKE_CURRENT_BINARY_DIR}/${TGT}.rc")
     set(RESOURCE_PATH_TMP "${RESOURCE_PATH}.in")
 
     # Extract major, minor and patch version from SENTRY_VERSION
@@ -17,5 +17,5 @@ function(sentry_add_version_resource TARGET FILE_DESCRIPTION)
     file(GENERATE OUTPUT ${RESOURCE_PATH} INPUT ${RESOURCE_PATH_TMP})
 
     # Finally add the generated resource file to the target sources
-    target_sources("${TARGET}" PRIVATE "${RESOURCE_PATH}")
+    target_sources("${TGT}" PRIVATE "${RESOURCE_PATH}")
 endfunction()
