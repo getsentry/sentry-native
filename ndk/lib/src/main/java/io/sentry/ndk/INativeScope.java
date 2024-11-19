d8230a8bff8157fb901181f0d8bfef0ad11cc7c3
Markus Hintersteiner <markus.hintersteiner@sentry.io>
2024-11-19 13:03:56 +0100

    [NDK] Properly apply code formatting (#1081)

--- ndk/lib/src/main/java/io/sentry/ndk/INativeScope.java
+++ ndk/lib/src/main/java/io/sentry/ndk/INativeScope.java
@@ -1,18 +1,18 @@
 package io.sentry.ndk;
 
 public interface INativeScope {
-    void setTag(String key, String value);
+  void setTag(String key, String value);
 
-    void removeTag(String key);
+  void removeTag(String key);
 
-    void setExtra(String key, String value);
+  void setExtra(String key, String value);
 
-    void removeExtra(String key);
+  void removeExtra(String key);
 
-    void setUser(String id, String email, String ipAddress, String username);
+  void setUser(String id, String email, String ipAddress, String username);
 
-    void removeUser();
+  void removeUser();
 
-    void addBreadcrumb(
-            String level, String message, String category, String type, String timestamp, String data);
+  void addBreadcrumb(
+      String level, String message, String category, String type, String timestamp, String data);
 }
--- ndk/lib/src/main/java/io/sentry/ndk/NativeModuleListLoader.java
+++ ndk/lib/src/main/java/io/sentry/ndk/NativeModuleListLoader.java
@@ -4,15 +4,15 @@ import org.jetbrains.annotations.Nullable;
 
 public final class NativeModuleListLoader {
 
-    public static native DebugImage[] nativeLoadModuleList();
+  public static native DebugImage[] nativeLoadModuleList();
 
-    public static native void nativeClearModuleList();
+  public static native void nativeClearModuleList();
 
-    public @Nullable DebugImage[] loadModuleList() {
-        return nativeLoadModuleList();
-    }
+  public @Nullable DebugImage[] loadModuleList() {
+    return nativeLoadModuleList();
+  }
 
-    public void clearModuleList() {
-        nativeClearModuleList();
-    }
+  public void clearModuleList() {
+    nativeClearModuleList();
+  }
 }
