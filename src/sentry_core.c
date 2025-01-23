57c38d9d7c5dc3e5aef0c848de0ec9ebfa8d1663
jkosberg <97864012+jkosberg@users.noreply.github.com>
2025-01-23 02:19:10 -0600

    fix: initialize local variable to prevent c4701 warning on windows arm (#1122)
    * Prevent c4701 warning on windows arm
    
    * comment that init has no semantic meaning
    
    ---------
    
    Co-authored-by: Mischan Toosarani-Hausberger <mischan@abovevacant.com>

--- src/sentry_core.c
+++ src/sentry_core.c
@@ -406,7 +406,9 @@ sentry__roll_dice(double probability)
 sentry_uuid_t
 sentry__capture_event(sentry_value_t event)
 {
-    sentry_uuid_t event_id;
+    // `event_id` is only used as an argument to pure output parameters.
+    // Initialization only happens to prevent compiler warnings.
+    sentry_uuid_t event_id = sentry_uuid_nil();
     sentry_envelope_t *envelope = NULL;
 
     bool was_captured = false;
