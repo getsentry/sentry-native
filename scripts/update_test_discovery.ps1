33742f0d92e5f4a4c7ccffe258a0b2956b61ab0b
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2025-01-29 17:10:40 +0100

    fix: allow test discovery to handle oneliner unit-tests

--- scripts/update_test_discovery.ps1
+++ scripts/update_test_discovery.ps1
@@ -1,7 +1,11 @@
-Get-ChildItem -Path 'tests/unit/*.c' | ForEach-Object {
+$lines = Get-ChildItem -Path 'tests/unit/*.c' | ForEach-Object {
     Get-Content $_.FullName | ForEach-Object {
         if ($_ -match 'SENTRY_TEST\(([^)]+)\)') {
-            $_ -replace 'SENTRY_TEST\(([^)]+)\)', 'XX($1)'
+            $_ -replace 'SENTRY_TEST\(([^)]+)\).*', 'XX($1)'
         }
     }
-} | Sort-Object | Where-Object { $_ -notmatch 'define' } | Get-Unique | Set-Content 'tests/unit/tests.inc'
+}
+
+[System.Array]::Sort($lines, [System.StringComparer]::Ordinal)
+
+$lines | Where-Object { $_ -notmatch 'define' } | Get-Unique | Set-Content 'tests/unit/tests.inc'
