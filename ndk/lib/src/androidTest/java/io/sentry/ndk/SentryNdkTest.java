6dbb550673de711cb7396880d16083675590b1a5
Markus Hintersteiner <markus.hintersteiner@sentry.io>
2025-03-19 07:45:09 +0100

    Apply code formatting

--- ndk/lib/src/androidTest/java/io/sentry/ndk/SentryNdkTest.java
+++ ndk/lib/src/androidTest/java/io/sentry/ndk/SentryNdkTest.java
@@ -4,13 +4,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import io.sentry.ndk.sample.NdkSample;
 import java.io.File;
 import java.io.IOException;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
-
-import io.sentry.ndk.sample.NdkSample;
 
 @RunWith(AndroidJUnit4.class)
 public class SentryNdkTest {
@@ -66,64 +65,64 @@ public class SentryNdkTest {
     // it does not crash
   }
 
-    @Test
-    public void transactionSampled() throws IOException {
-        final TemporaryFolder temporaryFolder = TemporaryFolder.builder().build();
-        temporaryFolder.create();
-        final File outboxPath = temporaryFolder.newFolder("outboxPath");
+  @Test
+  public void transactionSampled() throws IOException {
+    final TemporaryFolder temporaryFolder = TemporaryFolder.builder().build();
+    temporaryFolder.create();
+    final File outboxPath = temporaryFolder.newFolder("outboxPath");
 
-        final NdkOptions options =
-                new NdkOptions(
-                        "https://key@sentry.io/proj",
-                        true,
-                        outboxPath.getAbsolutePath(),
-                        "1.0.0",
-                        "production",
-                        "dist",
-                        100,
-                        "io.sentry.ndk",
-                        1.0f);
+    final NdkOptions options =
+        new NdkOptions(
+            "https://key@sentry.io/proj",
+            true,
+            outboxPath.getAbsolutePath(),
+            "1.0.0",
+            "production",
+            "dist",
+            100,
+            "io.sentry.ndk",
+            1.0f);
 
-        // when initialized
-        SentryNdk.init(options);
+    // when initialized
+    SentryNdk.init(options);
 
-        // then it does not crash
-        NdkSample.transaction();
+    // then it does not crash
+    NdkSample.transaction();
 
-        File[] files = outboxPath.listFiles();
-        assertNotNull(files);
-        assertEquals(1, files.length);
-        // TODO check file contents and if it contains "type":"transaction"
-    }
+    File[] files = outboxPath.listFiles();
+    assertNotNull(files);
+    assertEquals(1, files.length);
+    // TODO check file contents and if it contains "type":"transaction"
+  }
 
-    @Test
-    public void transactionNotSampled() throws IOException {
-        final TemporaryFolder temporaryFolder = TemporaryFolder.builder().build();
-        temporaryFolder.create();
-        final File outboxPath = temporaryFolder.newFolder("outboxPath");
+  @Test
+  public void transactionNotSampled() throws IOException {
+    final TemporaryFolder temporaryFolder = TemporaryFolder.builder().build();
+    temporaryFolder.create();
+    final File outboxPath = temporaryFolder.newFolder("outboxPath");
 
-        final NdkOptions options =
-                new NdkOptions(
-                        "https://key@sentry.io/proj",
-                        true,
-                        outboxPath.getAbsolutePath(),
-                        "1.0.0",
-                        "production",
-                        "dist",
-                        100,
-                        "io.sentry.ndk",
-                        0.0f);
+    final NdkOptions options =
+        new NdkOptions(
+            "https://key@sentry.io/proj",
+            true,
+            outboxPath.getAbsolutePath(),
+            "1.0.0",
+            "production",
+            "dist",
+            100,
+            "io.sentry.ndk",
+            0.0f);
 
-        // when initialized
-        SentryNdk.init(options);
+    // when initialized
+    SentryNdk.init(options);
 
-        // then it does not crash
-        NdkSample.transaction();
+    // then it does not crash
+    NdkSample.transaction();
 
-        File[] files = outboxPath.listFiles();
-        assertNotNull(files);
-        assertEquals(0, files.length);
-    }
+    File[] files = outboxPath.listFiles();
+    assertNotNull(files);
+    assertEquals(0, files.length);
+  }
 
   @Test
   public void shutdownDoesNotFailWhenNotInitialized() {
