28c6e94c277b82a617179329d92166cf7b43f3af
Ivan Dlugos <dlugos.ivan@gmail.com>
2025-04-25 10:29:53 +0200

    chore: cleanup comment

--- include/sentry.h
+++ include/sentry.h
@@ -804,9 +804,8 @@ SENTRY_API void sentry_options_set_network_connect_func(
     sentry_options_t *opts, void (*network_connect_func)(void));
 
 /**
- * If false (the default), the SDK won't include PII or other sensitive data in
- * the payload by default. For example, a pseudo-random identifier combining
- * device and application ID.
+ * If false (the default), the SDK won't add PII or other sensitive data to the
+ * payload. For example, a pseudo-random identifier combining device and app ID.
  */
 SENTRY_API void sentry_options_set_send_default_pii(
     sentry_options_t *opts, int value);
