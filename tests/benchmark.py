01b740a954bbb339f263773dbacabbbe99eb0c41
J-P Nurmi <jpnurmi@gmail.com>
2025-04-28 19:38:01 +0200

    test: benchmark SDK startup (#1209)
    * test: benchmark SDK startup
    
    * CI: permissions required for github-action-benchmark
    
    https://github.com/benchmark-action/github-action-benchmark?tab=readme-ov-file#charts-on-github-pages-1
    
    * tests/conftest.py: extract constants
    
    * Warmup run
    
    * Add min/max/mean/stddev
    
    * Filter out empty string from formatting
    
    * Move tests/unit/benchmark* -> tests/benchmark/
    
    * Uncomment github-action-benchmark

---
+++ tests/benchmark.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+
+import pytest
+
+from . import make_dsn, run
+
+
+@pytest.mark.parametrize(
+    "target,backend",
+    [
+        ("init", "inproc"),
+        ("init", "breakpad"),
+        ("init", "crashpad"),
+        ("backend", "inproc"),
+        ("backend", "breakpad"),
+        ("backend", "crashpad"),
+    ],
+)
+def test_benchmark(target, backend, cmake, httpserver, gbenchmark):
+    tmp_path = cmake(
+        ["sentry_benchmark"],
+        {
+            "SENTRY_BACKEND": backend,
+            "SENTRY_BUILD_BENCHMARKS": "ON",
+            "CMAKE_BUILD_TYPE": "Release",
+        },
+    )
+
+    env = dict(os.environ, SENTRY_DSN=make_dsn(httpserver))
+    benchmark_out = tmp_path / "benchmark.json"
+
+    for i in range(6):
+        # make sure we are isolated from previous runs
+        shutil.rmtree(tmp_path / ".sentry-native", ignore_errors=True)
+
+        run(
+            tmp_path,
+            "sentry_benchmark",
+            [f"--benchmark_filter={target}", f"--benchmark_out={benchmark_out}"],
+            check=True,
+            env=env,
+        )
+
+        # ignore warmup run
+        if i > 0:
+            gbenchmark(benchmark_out)
