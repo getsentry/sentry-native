168e51eb3c312286eb355eeb9aeacc598ed37f8b
JoshuaMoelans <60878493+JoshuaMoelans@users.noreply.github.com>
2025-06-17 15:14:10 +0200

    attach attributes to logs

--- src/sentry_logs.c
+++ src/sentry_logs.c
@@ -2,6 +2,7 @@
 #include "sentry_core.h"
 #include "sentry_envelope.h"
 #include "sentry_options.h"
+#include "sentry_scope.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -14,20 +15,106 @@ log_level_as_string(sentry_log_level_t level)
 {
     switch (level) {
     case SENTRY_LOG_LEVEL_TRACE:
-        return "TRACE";
+        return "trace";
     case SENTRY_LOG_LEVEL_DEBUG:
-        return "DEBUG";
+        return "debug";
     case SENTRY_LOG_LEVEL_INFO:
-        return "INFO";
+        return "info";
     case SENTRY_LOG_LEVEL_WARN:
-        return "WARN";
+        return "warn";
     case SENTRY_LOG_LEVEL_ERROR:
-        return "ERROR";
+        return "error";
     case SENTRY_LOG_LEVEL_FATAL:
-        return "FATAL";
+        return "fatal";
     default:
         return "UNKNOWN";
     }
+}
+
+static sentry_value_t
+construct_log(sentry_log_level_t level, const char *message, va_list args)
+{
+    sentry_value_t log = sentry_value_new_object();
+    sentry_value_t attributes = sentry_value_new_object();
+
+    sentry_value_set_by_key(log, "body", sentry_value_new_string(message));
+    sentry_value_set_by_key(
+        log, "level", sentry_value_new_string(log_level_as_string(level)));
+
+    // timestamp in seconds
+    sentry_value_set_by_key(log, "timestamp",
+        sentry_value_new_double(sentry__usec_time() / 1000000.0));
+
+    SENTRY_WITH_SCOPE_MUT (scope) {
+        sentry_value_set_by_key(log, "trace_id",
+            sentry__value_clone(sentry_value_get_by_key(
+                sentry_value_get_by_key(scope->propagation_context, "trace"),
+                "trace_id")));
+
+        sentry_value_t parent_span_id = sentry_value_new_object();
+        if (scope->transaction_object) {
+            sentry_value_set_by_key(parent_span_id, "value",
+                sentry__value_clone(sentry_value_get_by_key(
+                    scope->transaction_object->inner, "span_id")));
+        }
+
+        if (scope->span) {
+            sentry_value_set_by_key(parent_span_id, "value",
+                sentry__value_clone(
+                    sentry_value_get_by_key(scope->span->inner, "span_id")));
+        }
+        sentry_value_set_by_key(
+            parent_span_id, "type", sentry_value_new_string("string"));
+        // TODO should we only add if either exists?
+        sentry_value_set_by_key(
+            attributes, "sentry.trace.parent_span_id", parent_span_id);
+    }
+
+    SENTRY_WITH_OPTIONS (options) {
+        if (options->environment) {
+            sentry_value_t environment = sentry_value_new_object();
+            sentry_value_set_by_key(environment, "value",
+                sentry_value_new_string(options->environment));
+            sentry_value_set_by_key(
+                environment, "type", sentry_value_new_string("string"));
+            sentry_value_set_by_key(
+                attributes, "sentry.environment", environment);
+        }
+        if (options->release) {
+            sentry_value_t release = sentry_value_new_object();
+            sentry_value_set_by_key(
+                release, "value", sentry_value_new_string(options->release));
+            sentry_value_set_by_key(
+                release, "type", sentry_value_new_string("string"));
+            sentry_value_set_by_key(attributes, "sentry.release", release);
+        }
+    }
+
+    sentry_value_t sdk_name = sentry_value_new_object();
+    sentry_value_set_by_key(
+        sdk_name, "value", sentry_value_new_string("sentry.native"));
+    sentry_value_set_by_key(
+        sdk_name, "type", sentry_value_new_string("string"));
+    sentry_value_set_by_key(attributes, "sentry.sdk.name", sdk_name);
+
+    sentry_value_t sdk_version = sentry_value_new_object();
+    sentry_value_set_by_key(
+        sdk_version, "value", sentry_value_new_string(sentry_sdk_version()));
+    sentry_value_set_by_key(
+        sdk_version, "type", sentry_value_new_string("string"));
+    sentry_value_set_by_key(attributes, "sentry.sdk.name", sdk_version);
+
+    sentry_value_t message_template = sentry_value_new_object();
+    sentry_value_set_by_key(
+        message_template, "value", sentry_value_new_string(message));
+    sentry_value_set_by_key(
+        message_template, "type", sentry_value_new_string("string"));
+    sentry_value_set_by_key(
+        attributes, "sentry.message.template", message_template);
+
+    sentry_value_set_by_key(log, "attributes", attributes);
+
+    return log;
 }
 
 void
@@ -40,18 +127,7 @@ sentry__logs_log(sentry_log_level_t level, const char *message, va_list args)
     SENTRY_INFOF("Logging level: %i\n", level);
     vprintf(message, args);
     // create log from message
-    sentry_value_t log = sentry_value_new_object();
-    sentry_value_set_by_key(log, "body", sentry_value_new_string(message));
-    sentry_value_set_by_key(
-        log, "level", sentry_value_new_string(log_level_as_string(level)));
-    // TODO add actual trace_id
-    sentry_value_set_by_key(log, "trace_id",
-        sentry_value_new_string("5b8efff798038103d269b633813fc60c"));
-    // timestamp in seconds
-    sentry_value_set_by_key(log, "timestamp",
-        sentry_value_new_double(sentry__usec_time() / 1000000.0));
-    // TODO add log attributes
-    //    https://develop.sentry.dev/sdk/telemetry/logs/#default-attributes
+    sentry_value_t log = construct_log(level, message, args);
 
     // TODO split up the code below for batched log sending
     //    e.g. could we store logs on the scope?
@@ -62,6 +138,7 @@ sentry__logs_log(sentry_log_level_t level, const char *message, va_list args)
     // sending of the envelope
     sentry_envelope_t *envelope = sentry__envelope_new();
     sentry__envelope_add_logs(envelope, logs);
+    // TODO remove debug write to file below
     sentry_envelope_write_to_file(envelope, "logs_envelope.json");
     SENTRY_WITH_OPTIONS (options) {
         sentry__capture_envelope(options->transport, envelope);
