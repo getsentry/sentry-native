37ece60ae70e60a4d31b841a3b5449092ca828d2
JoshuaMoelans <60878493+JoshuaMoelans@users.noreply.github.com>
2025-06-24 16:43:11 +0200

    add SENTRY_LEVEL_TRACE to sentry_logger

--- src/sentry_logger.c
+++ src/sentry_logger.c
@@ -69,6 +69,8 @@ const char *
 sentry__logger_describe(sentry_level_t level)
 {
     switch (level) {
+    case SENTRY_LEVEL_TRACE:
+        return "TRACE ";
     case SENTRY_LEVEL_DEBUG:
         return "DEBUG ";
     case SENTRY_LEVEL_INFO:
