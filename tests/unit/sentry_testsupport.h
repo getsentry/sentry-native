28e2f610bf5caaa945743fcaef513df850769aa3
Ivan Dlugos <6349682+vaind@users.noreply.github.com>
2025-06-25 10:49:46 +0200

    Update tests/unit/sentry_testsupport.h

--- tests/unit/sentry_testsupport.h
+++ tests/unit/sentry_testsupport.h
@@ -75,7 +75,7 @@
         sentry_options_t *Varname = sentry_options_new();                      \
         TEST_ASSERT(!!Varname);                                                \
         sentry_options_set_database_path(                                      \
-            Varname, SENTRY_TEST_PATH_PREFIX ".sentry-native");
+            Varname, SENTRY_TEST_PATH_PREFIX ".sentry-native")
 #else
 #    define SENTRY_TEST_PATH_PREFIX ""
 #    define SENTRY_TEST_OPTIONS_NEW(Varname)                                   \
