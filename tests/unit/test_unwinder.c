0d8bf6956c012086d0f454f2341cf05a2b706d1a
J-P Nurmi <jpnurmi@gmail.com>
2025-07-12 07:26:10 +0200

    fix skip_test

--- tests/unit/test_unwinder.c
+++ tests/unit/test_unwinder.c
@@ -39,7 +39,7 @@ find_frame(const sentry_frame_info_t *info, void *data)
 SENTRY_TEST(unwinder)
 {
 #if defined(SENTRY_PLATFORM_NX) || defined(SENTRY_PLATFORM_XBOX)
-    return SKIP_TEST();
+    SKIP_TEST();
 #endif
     void *backtrace1[MAX_FRAMES] = { 0 };
     size_t frame_count1 = invoke_unwinder(backtrace1);
