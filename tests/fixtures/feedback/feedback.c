1b411d9652bafeea7a062bb5e5bc8e22b41909f7
J-P Nurmi <jpnurmi@gmail.com>
2025-07-22 11:13:10 +0200

    update feedback.c

--- tests/fixtures/feedback/feedback.c
+++ tests/fixtures/feedback/feedback.c
@@ -23,39 +23,27 @@ main(int argc, char *argv[])
     }
 
     char *path = argv[1];
-    FILE *file = fopen(path, "r");
-    if (!file) {
+    sentry_envelope_t *envelope = sentry_envelope_read_from_file(path);
+    if (!envelope) {
         fprintf(stderr, "ERROR: %s (%s)\n", path, strerror(errno));
         return EXIT_FAILURE;
     }
 
-    char header[768];
-    if (!fgets(header, sizeof(header), file)) {
-        fprintf(stderr, "ERROR: %s (%s)\n", path, strerror(errno));
-        fclose(file);
-        return EXIT_FAILURE;
-    }
-    fclose(file);
-
-    char dsn[513];
-    char event_id[37];
-    int parsed = sscanf(header,
-        "{\"dsn\":\"%512[^\"]\",\"event_id\":\"%36[^\"]\"}", dsn, event_id);
-    if (parsed != 2) {
-        fprintf(stderr, "ERROR: Failed to parse header\n");
-    }
+    sentry_value_t dsn = sentry_envelope_get_header(envelope, "dsn");
+    sentry_value_t event_id = sentry_envelope_get_header(envelope, "event_id");
 
     sentry_options_t *options = sentry_options_new();
     sentry_options_set_backend(options, NULL);
-    sentry_options_set_dsn(options, dsn);
+    sentry_options_set_dsn(options, sentry_value_as_string(dsn));
     sentry_options_set_debug(options, true);
     sentry_init(options);
 
-    sentry_uuid_t uuid = sentry_uuid_from_string(event_id);
+    sentry_uuid_t uuid
+        = sentry_uuid_from_string(sentry_value_as_string(event_id));
     sentry_value_t feedback = sentry_value_new_feedback(
         "some-message", "some-email", "some-name", &uuid);
 
-    sentry_capture_envelope(sentry_envelope_read_from_file(path));
+    sentry_capture_envelope(envelope);
     sentry_capture_feedback(feedback);
 
     sentry_close();
