a7d36edc7d300316aa61ce30b0c0abbe18b70cbb
JoshuaMoelans <60878493+JoshuaMoelans@users.noreply.github.com>
2025-07-31 10:50:47 +0200

    add explanation/link for writing uint64 as string

--- src/sentry_json.c
+++ src/sentry_json.c
@@ -382,7 +382,9 @@ void
 sentry__jsonwriter_write_uint64(sentry_jsonwriter_t *jw, uint64_t val)
 {
     // TODO currently all uint64 values should be sent as strings; update when
-    // this changes
+    // this changes and 64-bit unsigned integers can be ingested properly
+    // see
+    // https://github.com/getsentry/sentry-native/pull/1326#discussion_r2241965997
     if (can_write_item(jw)) {
         char buf[26];
         snprintf(buf, sizeof(buf), "\"%" PRIu64 "\"", val);
