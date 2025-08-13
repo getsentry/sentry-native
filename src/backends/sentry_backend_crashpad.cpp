getsentry/sentry-native

commit 792b4472450705fdb409ce75239d3f90917bdba0

Bump crashpad & keep __sentry-event

src/backends/sentry_backend_crashpad.cpp
@@ -213,27 +213,19 @@ flush_scope_to_event(const sentry_path_t *event_path,
 }
 
 static void
-flush_scope_to_report(const sentry_path_t *event_path,
-    const sentry_options_t *options, sentry_value_t crash_event)
+flush_scope_to_report(
+    const sentry_path_t *report_path, const sentry_options_t *options)
 {
-    SENTRY_WITH_SCOPE (scope) {
-        // we want the scope without any modules or breadcrumbs
-        sentry__scope_apply_to_event(
-            scope, options, crash_event, SENTRY_SCOPE_NONE);
-    }
-
     sentry_envelope_t *envelope = sentry__envelope_new();
     if (!envelope) {
-        sentry_value_decref(crash_event);
         return;
     }
-    sentry__envelope_add_event(envelope, crash_event);
     if (options->session) {
         sentry__envelope_add_session(envelope, options->session);
     }
 
     if (sentry__path_create_dir_all(options->run->report_path) != 0
-        || sentry_envelope_write_to_path(envelope, event_path) != 0) {
+        || sentry_envelope_write_to_path(envelope, report_path) != 0) {
         SENTRY_WARN("flushing scope to report failed");
     }
     sentry_envelope_free(envelope);
@@ -271,10 +263,9 @@ crashpad_backend_flush_scope(
     sentry_value_set_by_key(
         event, "level", sentry__value_new_level(SENTRY_LEVEL_FATAL));
 
+    flush_scope_to_event(data->event_path, options, event);
     if (data->report_path) {
-        flush_scope_to_report(data->report_path, options, event);
-    } else {
-        flush_scope_to_event(data->event_path, options, event);
+        flush_scope_to_report(data->report_path, options);
     }
     data->scope_flush.store(false, std::memory_order_release);
 #endif
@@ -298,10 +289,9 @@ flush_scope_from_handler(
     }
 
     // now we are the sole flusher and can flush into the crash report/event
+    flush_scope_to_event(state->event_path, options, crash_event);
     if (state->report_path) {
-        flush_scope_to_report(state->report_path, options, crash_event);
-    } else {
-        flush_scope_to_event(state->event_path, options, crash_event);
+        flush_scope_to_report(state->report_path, options);
     }
 }
 

commit 5681c61b25bfa61e3fdf39ba3fef69eca8921d6c

Fix missing event ID header in Crashpad reports
A regression caused by 792b4472450705fdb409ce75239d3f90917bdba0
as we're no longer adding event to the external crash report but
let Crashpad have the usual msg-packed __sentry-event.

src/backends/sentry_backend_crashpad.cpp
@@ -212,21 +212,24 @@ flush_scope_to_event(const sentry_path_t *event_path,
     }
 }
 
+// Prepares an envelope with DSN, event ID, and session if available, for an
+// external crash reporter.
 static void
-flush_scope_to_report(
-    const sentry_path_t *report_path, const sentry_options_t *options)
+flush_external_crash_report(const sentry_path_t *report_path,
+    const sentry_options_t *options, const sentry_uuid_t *crash_event_id)
 {
     sentry_envelope_t *envelope = sentry__envelope_new();
     if (!envelope) {
         return;
     }
+    sentry__envelope_set_event_id(envelope, crash_event_id);
     if (options->session) {
         sentry__envelope_add_session(envelope, options->session);
     }
 
     if (sentry__path_create_dir_all(options->run->report_path) != 0
         || sentry_envelope_write_to_path(envelope, report_path) != 0) {
-        SENTRY_WARN("flushing scope to report failed");
+        SENTRY_WARN("flushing external crash report failed");
     }
     sentry_envelope_free(envelope);
 }
@@ -265,7 +268,8 @@ crashpad_backend_flush_scope(
 
     flush_scope_to_event(data->event_path, options, event);
     if (data->report_path) {
-        flush_scope_to_report(data->report_path, options);
+        flush_external_crash_report(
+            data->report_path, options, &data->crash_event_id);
     }
     data->scope_flush.store(false, std::memory_order_release);
 #endif
@@ -291,7 +295,8 @@ flush_scope_from_handler(
     // now we are the sole flusher and can flush into the crash report/event
     flush_scope_to_event(state->event_path, options, crash_event);
     if (state->report_path) {
-        flush_scope_to_report(state->report_path, options);
+        flush_external_crash_report(
+            state->report_path, options, &state->crash_event_id);
     }
 }
 

src/sentry_envelope.c
@@ -214,6 +214,14 @@ sentry__envelope_get_event_id(const sentry_envelope_t *envelope)
         sentry_value_get_by_key(envelope->contents.items.headers, "event_id")));
 }
 
+void
+sentry__envelope_set_event_id(
+    sentry_envelope_t *envelope, const sentry_uuid_t *event_id)
+{
+    sentry_value_t value = sentry__value_new_uuid(event_id);
+    sentry__envelope_set_header(envelope, "event_id", value);
+}
+
 sentry_value_t
 sentry_envelope_get_event(const sentry_envelope_t *envelope)
 {
@@ -260,7 +268,8 @@ sentry__envelope_add_event(sentry_envelope_t *envelope, sentry_value_t event)
         return NULL;
     }
 
-    sentry_value_t event_id = sentry__ensure_event_id(event, NULL);
+    sentry_uuid_t event_id;
+    sentry__ensure_event_id(event, &event_id);
 
     item->event = event;
     sentry__jsonwriter_write_value(jw, event);
@@ -271,8 +280,7 @@ sentry__envelope_add_event(sentry_envelope_t *envelope, sentry_value_t event)
     sentry_value_t length = sentry_value_new_int32((int32_t)item->payload_len);
     sentry__envelope_item_set_header(item, "length", length);
 
-    sentry_value_incref(event_id);
-    sentry__envelope_set_header(envelope, "event_id", event_id);
+    sentry__envelope_set_event_id(envelope, &event_id);
 
     double traces_sample_rate = 0.0;
     SENTRY_WITH_OPTIONS (options) {
@@ -342,7 +350,8 @@ sentry__envelope_add_transaction(
         return NULL;
     }
 
-    sentry_value_t event_id = sentry__ensure_event_id(transaction, NULL);
+    sentry_uuid_t event_id;
+    sentry__ensure_event_id(transaction, &event_id);
 
     item->event = transaction;
     sentry__jsonwriter_write_value(jw, transaction);
@@ -353,8 +362,7 @@ sentry__envelope_add_transaction(
     sentry_value_t length = sentry_value_new_int32((int32_t)item->payload_len);
     sentry__envelope_item_set_header(item, "length", length);
 
-    sentry_value_incref(event_id);
-    sentry__envelope_set_header(envelope, "event_id", event_id);
+    sentry__envelope_set_event_id(envelope, &event_id);
 
     sentry_value_t dsc = sentry_value_new_null();
 
@@ -415,7 +423,8 @@ sentry__envelope_add_user_report(
         return NULL;
     }
 
-    sentry_value_t event_id = sentry__ensure_event_id(user_report, NULL);
+    sentry_uuid_t event_id;
+    sentry__ensure_event_id(user_report, &event_id);
 
     sentry__jsonwriter_write_value(jw, user_report);
     item->payload = sentry__jsonwriter_into_string(jw, &item->payload_len);
@@ -425,8 +434,7 @@ sentry__envelope_add_user_report(
     sentry_value_t length = sentry_value_new_int32((int32_t)item->payload_len);
     sentry__envelope_item_set_header(item, "length", length);
 
-    sentry_value_incref(event_id);
-    sentry__envelope_set_header(envelope, "event_id", event_id);
+    sentry__envelope_set_event_id(envelope, &event_id);
 
     return item;
 }

src/sentry_envelope.h
@@ -32,6 +32,12 @@ sentry_envelope_t *sentry__envelope_from_path(const sentry_path_t *path);
 sentry_uuid_t sentry__envelope_get_event_id(const sentry_envelope_t *envelope);
 
 /**
+ * Set the event ID header for this envelope.
+ */
+void sentry__envelope_set_event_id(
+    sentry_envelope_t *envelope, const sentry_uuid_t *event_id);
+
+/**
  * Add an event to this envelope.
  */
 sentry_envelope_item_t *sentry__envelope_add_event(

tests/assertions.py
@@ -71,6 +71,7 @@ def assert_meta(
     transaction_data=None,
     sdk_override=None,
 ):
+    assert envelope.headers["event_id"]
     event = envelope.get_event()
     assert_event_meta(
         event, release, integration, transaction, transaction_data, sdk_override