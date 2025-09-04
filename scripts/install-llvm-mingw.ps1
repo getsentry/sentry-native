803590ff96142604b49c4bfb0f7c99a34eb85458
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2025-09-04 14:12:18 +0200

    fix `LLVM_MINGW_INSTALL_PATH` to be referenced locally rather than $env

--- scripts/install-llvm-mingw.ps1
+++ scripts/install-llvm-mingw.ps1
@@ -65,8 +65,9 @@ $cmakeDefines += @(
   "-DCMAKE_C_COMPILER=$($env:MINGW_PKG_PREFIX)-gcc"
   "-DCMAKE_CXX_COMPILER=$($env:MINGW_PKG_PREFIX)-g++"
   "-DCMAKE_RC_COMPILER=$($env:MINGW_PKG_PREFIX)-windres"
-  "-DCMAKE_ASM_MASM_COMPILER:FILEPATH=$($env:LLVM_MINGW_INSTALL_PATH -replace '\\','/')/bin/$($env:MINGW_ASM_MASM_COMPILER).exe"
+  "-DCMAKE_ASM_MASM_COMPILER:FILEPATH=$($LLVM_MINGW_INSTALL_PATH -replace '\\','/')/bin/$($env:MINGW_ASM_MASM_COMPILER).exe"
   "-DCMAKE_ASM_MASM_FLAGS=$env:MINGW_ASM_MASM_FLAGS"
 )
 
 "CMAKE_DEFINES=$($cmakeDefines -join ' ')" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+
