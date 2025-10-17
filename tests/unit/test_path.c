2edb298427cb5b2bb296db86af9633a4b6ace4db
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2025-10-17 08:43:42 +0200

    reintroduce wide string comparisons in unit-tests

--- tests/unit/test_path.c
+++ tests/unit/test_path.c
@@ -99,7 +99,10 @@ SENTRY_TEST(path_joining_windows)
     sentry_path_t *winpath = sentry__path_new("foo\\bar\\baz.txt");
     sentry_path_t *awinpath = sentry__path_from_str("foo\\bar\\baz.txt");
     sentry_path_t *cpath = sentry__path_from_str("C:\\foo\\bar\\baz.txt");
-    sentry_path_t *joined;
+    TEST_ASSERT(!!path);
+    TEST_ASSERT(!!winpath);
+    TEST_ASSERT(!!awinpath);
+    TEST_ASSERT(!!cpath);
 
     TEST_CHECK(_stricmp(path->path, "foo/bar/baz.txt") == 0);
     TEST_CHECK(_stricmp(sentry__path_filename(path), "baz.txt") == 0);
@@ -107,26 +110,39 @@ SENTRY_TEST(path_joining_windows)
     TEST_CHECK(_stricmp(awinpath->path, "foo\\bar\\baz.txt") == 0);
     TEST_CHECK(_stricmp(sentry__path_filename(winpath), "baz.txt") == 0);
 
-    joined = sentry__path_join_str(path, "extra");
+    TEST_CHECK(_wcsicmp(path->path_w, L"foo/bar/baz.txt") == 0);
+    TEST_CHECK(_wcsicmp(sentry__path_filename_w(path), L"baz.txt") == 0);
+    TEST_CHECK(_wcsicmp(winpath->path_w, L"foo\\bar\\baz.txt") == 0);
+    TEST_CHECK(_wcsicmp(awinpath->path_w, L"foo\\bar\\baz.txt") == 0);
+    TEST_CHECK(_wcsicmp(sentry__path_filename_w(winpath), L"baz.txt") == 0);
+
+    sentry_path_t *joined = sentry__path_join_str(path, "extra");
     TEST_CHECK(_stricmp(joined->path, "foo/bar/baz.txt\\extra") == 0);
     TEST_CHECK(_stricmp(sentry__path_filename(joined), "extra") == 0);
+    TEST_CHECK(_wcsicmp(joined->path_w, L"foo/bar/baz.txt\\extra") == 0);
+    TEST_CHECK(_wcsicmp(sentry__path_filename_w(joined), L"extra") == 0);
     sentry__path_free(joined);
 
     joined = sentry__path_join_str(path, "/root/path");
     TEST_CHECK(_stricmp(joined->path, "/root/path") == 0);
     TEST_CHECK(_stricmp(sentry__path_filename(joined), "path") == 0);
+    TEST_CHECK(_wcsicmp(joined->path_w, L"/root/path") == 0);
+    TEST_CHECK(_wcsicmp(sentry__path_filename_w(joined), L"path") == 0);
     sentry__path_free(joined);
 
     joined = sentry__path_join_str(cpath, "/root/path");
     TEST_CHECK(_stricmp(joined->path, "C:/root/path") == 0);
+    TEST_CHECK(_wcsicmp(joined->path_w, L"C:/root/path") == 0);
     sentry__path_free(joined);
 
     joined = sentry__path_join_str(cpath, "D:\\root\\path");
     TEST_CHECK(_stricmp(joined->path, "D:\\root\\path") == 0);
+    TEST_CHECK(_wcsicmp(joined->path_w, L"D:\\root\\path") == 0);
     sentry__path_free(joined);
 
     joined = sentry__path_join_str(cpath, "\\root\\path");
     TEST_CHECK(_stricmp(joined->path, "C:\\root\\path") == 0);
+    TEST_CHECK(_wcsicmp(joined->path_w, L"C:\\root\\path") == 0);
     sentry__path_free(joined);
 
     sentry__path_free(cpath);
