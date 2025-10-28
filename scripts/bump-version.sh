b5a9bafb9c6797d0d1818f595a325df890fd923f
JoshuaMoelans <60878493+JoshuaMoelans@users.noreply.github.com>
2025-10-28 16:20:50 +0100

    update bump-version.sh to handle sentry.h spaces

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -eux
 
-if [ "$(uname -s)" != "Linux" ]; then
-    echo "Please use the GitHub Action."
-    exit 1
-fi
+#if [ "$(uname -s)" != "Linux" ]; then
+#    echo "Please use the GitHub Action."
+#    exit 1
+#fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/..
@@ -14,7 +14,7 @@ NEW_VERSION="$2"
 
 echo "Bumping version: ${NEW_VERSION}"
 
-perl -pi -e "s/^#define SENTRY_SDK_VERSION.*/#define SENTRY_SDK_VERSION \"${NEW_VERSION}\"/" include/sentry.h
+perl -pi -e "s/^#    define SENTRY_SDK_VERSION.*/#    define SENTRY_SDK_VERSION \"${NEW_VERSION}\"/" include/sentry.h
 perl -pi -e "s/\"version\": \"[^\"]+\"/\"version\": \"${NEW_VERSION}\"/" tests/assertions.py
 perl -pi -e "s/sentry.native\/[^\"]+\"/sentry.native\/${NEW_VERSION}\"/" tests/test_integration_http.py
 perl -pi -e "s/^versionName\=.*/versionName\=${NEW_VERSION}/" ndk/gradle.properties
