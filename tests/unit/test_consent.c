9ad717ab2bee61608d53e170ea69530959384796
Ivan Tustanivskyi <tustanivsky@gmail.com>
2025-11-05 08:53:20 +0200

    Add test for init without explicit consent configuration

--- tests/unit/test_consent.c
+++ tests/unit/test_consent.c
@@ -72,7 +72,15 @@ SENTRY_TEST(query_consent_requirement)
     TEST_ASSERT(!!path);
     sentry__path_remove_all(path);
 
-    // Test when consent is NOT required
+    // Test default behavior when require_user_consent is not set
+    sentry_options_t *opts = sentry_options_new();
+    sentry_options_set_database_path(opts, SENTRY_TEST_PATH_PREFIX ".test-db");
+    sentry_options_set_dsn(opts, "http://foo@127.0.0.1/42");
+    sentry_init(opts);
+    TEST_CHECK_INT_EQUAL(sentry_user_consent_is_required(), 0);
+    sentry_close();
+
+    // Test when consent is explicitly NOT required
     init_not_consenting_sentry();
     TEST_CHECK_INT_EQUAL(sentry_user_consent_is_required(), 0);
     sentry_close();
