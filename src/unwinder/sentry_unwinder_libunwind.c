df9272853311ae5fbd9bba0299e3f76ffc8221ad
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2025-11-14 20:35:09 +0100

    eliminate the logs and ensure we never walk the callers if the SP is in unmapped memory

--- src/unwinder/sentry_unwinder_libunwind.c
+++ src/unwinder/sentry_unwinder_libunwind.c
@@ -3,7 +3,6 @@
 #define UNW_LOCAL_ONLY
 #include <libunwind.h>
 #include <stdio.h>
-#include <unistd.h>
 
 typedef struct {
     uintptr_t lo, hi;
@@ -11,40 +10,33 @@ typedef struct {
 
 /**
  * Looks up the memory range for a given pointer in /proc/self/maps.
- * `range` is an output parameter. Returns 0 on success.
+ * `range` is an output parameter. Returns `true` if a range was found.
  * Note: it is safe to use this function as long as we are running in a healthy
  * thread or in the handler thread. The function is unsafe for a signal handler.
  */
-static int
+static bool
 find_mem_range(uintptr_t ptr, mem_range_t *range)
 {
+    bool found = false;
     FILE *fp = fopen("/proc/self/maps", "r");
     if (!fp) {
-        return 1;
+        return found;
     }
-    int result = 1;
     char line[512];
     while (fgets(line, sizeof line, fp)) {
         unsigned long lo, hi;
-        SENTRY_INFOF("%s", line);
         if (sscanf(line, "%lx-%lx", &lo, &hi) == 2) {
             // our bounds are [lo, hi)
             if (ptr >= lo && ptr < hi) {
                 range->lo = (uintptr_t)lo;
                 range->hi = (uintptr_t)hi;
-                SENTRY_INFOF("Found ptr (0x%" PRIx64
-                             ") in the range %0x%" PRIx64 "-%0x%" PRIx64,
-                    ptr, lo, hi);
-                result = 0;
+                found = true;
                 break;
             }
         }
     }
     fclose(fp);
-    if (result) {
-        SENTRY_WARNF("Failed to find range for ptr (0x%" PRIx64 ")", ptr);
-    }
-    return result;
+    return found;
 }
 
 size_t
@@ -88,7 +80,7 @@ sentry__unwind_stack_libunwind(
     }
 
     size_t n = 0;
-    // get the first frame
+    // get the first frame and stack pointer
     unw_word_t ip = 0;
     if (unw_get_reg(&cursor, UNW_REG_IP, &ip) < 0) {
         return n;
@@ -99,36 +91,20 @@ sentry__unwind_stack_libunwind(
     unw_word_t sp = 0;
     (void)unw_get_reg(&cursor, UNW_REG_SP, &sp);
 
+    // ensure we have a valid stack pointer otherwise we only send the top frame
     mem_range_t stack = { 0, 0 };
-    int have_bounds = 0;
-    long page_size = 0;
-    if (uctx) {
-        if (find_mem_range((uintptr_t)sp, &stack) == 0) {
-            have_bounds = 1;
-            page_size = sysconf(_SC_PAGESIZE);
-            if (page_size <= 0) {
-                page_size = 4096;
-            }
-        }
+    if (uctx && !find_mem_range((uintptr_t)sp, &stack) == 0) {
+        SENTRY_WARNF("unwinder: SP (%p) is in unmapped memory likely due to stack overflow",
+            (void *)sp);
+        return n;
     }
 
     // walk the callers
     unw_word_t prev_ip = ip;
     unw_word_t prev_sp = sp;
-    size_t step_idx = 0;
-    while (n < max_frames) {
-        SENTRY_DEBUGF(
-            "unwind: about to unw_step, step=%zu, prev_ip=%p prev_sp=%p",
-            step_idx, (void *)prev_ip, (void *)prev_sp);
-        if (unw_step(&cursor) <= 0) {
-            SENTRY_DEBUGF("unwind: unw_step failed at step=%zu", step_idx);
-            break;
-        }
-        SENTRY_DEBUGF("unwind: unw_step success at step=%zu", step_idx);
-
+    while (n < max_frames && unw_step(&cursor) > 0) {
         // stop the walk if we fail to read IP
         if (unw_get_reg(&cursor, UNW_REG_IP, &ip) < 0) {
-            SENTRY_DEBUGF("unwind: no progress at step=%zu", step_idx);
             break;
         }
         // SP is optional for progress
@@ -139,18 +115,9 @@ sentry__unwind_stack_libunwind(
             break;
         }
 
-        if (have_bounds) {
-            intptr_t d_lo = (intptr_t)((uintptr_t)sp - stack.lo);
-            intptr_t d_hi = (intptr_t)((uintptr_t)stack.hi - sp);
-            SENTRY_DEBUGF(
-                "unwind: unw_step %zu: ip=%p, sp=%p, d_lo=%zd, d_hi=%zd", n,
-                (void *)ip, (void *)sp, d_lo, d_hi);
-        }
-
         prev_ip = ip;
         prev_sp = sp;
         ptrs[n++] = (void *)ip;
-        step_idx++;
     }
     return n;
 }
