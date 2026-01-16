946b7c9c3cf41cc6a007e8aea25e572c90ef182c
JoshuaMoelans <60878493+JoshuaMoelans@users.noreply.github.com>
2026-01-16 14:16:31 +0100

    update to __NINTENDO__

--- include/sentry.h
+++ include/sentry.h
@@ -83,9 +83,7 @@ extern "C" {
 /* IBM i PASE is also counted as AIX */
 #    define SENTRY_PLATFORM_AIX
 #    define SENTRY_PLATFORM_UNIX
-#elif defined(__NX__)
-#    define SENTRY_PLATFORM_NX
-#elif defined(__OUNCE__)
+#elif defined(__NINTENDO__)
 #    define SENTRY_PLATFORM_NX
 #else
 #    error unsupported platform
