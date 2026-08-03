fb977aefacabe09de4b8d1666e15d8b786e7afa3
J-P Nurmi <jpnurmi@gmail.com>
2026-08-03 12:18:59 +0200

    try init_priority for gcc & clang

--- tests/fixtures/early_init/early_init.cpp
+++ tests/fixtures/early_init/early_init.cpp
@@ -8,15 +8,21 @@ do_early_init()
     return sentry_init(options);
 }
 
+struct EarlyInit {
+    int result = do_early_init();
+};
+
 #if defined(_MSC_VER)
 // call sentry_init() as early as possible, before other static initializers
 #    pragma init_seg(lib)
+#elif defined(__GNUC__) || defined(__clang__)
+__attribute__((init_priority(101)))
 #endif
-static int early_init = do_early_init();
+static EarlyInit early_init;
 
 int
 main()
 {
     sentry_close();
-    return early_init;
+    return early_init.result;
 }
