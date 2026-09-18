8f243e24bd5b13d1bf292f54817026cd94d98a9c
J-P Nurmi <jpnurmi@gmail.com>
2026-09-18 14:46:18 +0200

    respect ANDROID_NDK in Gradle configuration

--- ndk/lib/build.gradle.kts
+++ ndk/lib/build.gradle.kts
@@ -14,7 +14,7 @@ val sanitizer =
 android {
     compileSdk = 37
     // retain AGP 8.7.3's default NDK to avoid changing the compiler and libc++ in a hotfix
-    ndkVersion = "27.0.12077973"
+    ndkVersion = System.getenv("ANDROID_NDK") ?: "27.0.12077973"
     namespace = "io.sentry.ndk"
 
     testBuildType = "debug"
@@ -54,7 +54,6 @@ android {
     }
 
     if (sanitizer != null) {
-        System.getenv("ANDROID_NDK")?.let { ndkVersion = it }
         sourceSets {
             getByName("androidTest") {
                 jniLibs.srcDir("build/$sanitizer/jniLibs")
